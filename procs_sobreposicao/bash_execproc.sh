6762943ce49c5c1b3e1e0a3b3dfbd0704a019e4b
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-01-20 13:19:26 -0300

    Update bash_execproc.sh

--- procs_sobreposicao/bash_execproc.sh
+++ procs_sobreposicao/bash_execproc.sh
@@ -1,6 +1,6 @@
 userName=postgres
 databaseName=malha_fundiaria
-numProc=14
+numProc=120
 procName=aux
 
 
@@ -11,9 +11,40 @@ psql -U $userName -d $databaseName -f ${procName}_1.sql
 for ((i=0; i < ${numProc}; i++))
 do
     psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_2.sql &
-done
+done > log_autointersection 2>&1
 wait
 
+
+#If sql 2 exists execute it
+for ((i=0; i < ${numProc}; i++))
+do
+    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_3.sql &
+done > log_inputs 2>&1
+wait
+
+
+procName=proc5_overlay
+
+#If sql 2 exists execute it
+for ((i=0; i < ${numProc}; i++))
+do
+    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f procs/${procName}_2_17.sql &
+done > log_massas 2>&1
+wait
+
+#If sql 2 exists execute it
+for ((i=0; i < ${numProc}; i++))
+do
+    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f procs/${procName}_2_18.sql &
+done > log_fronteira 2>&1
+wait
+
+#If sql 2 exists execute it
+for ((i=0; i < ${numProc}; i++))
+do
+    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f procs/${procName}_2_19.sql &
+done > log_sicar 2>&1
+wait
 
 
 
