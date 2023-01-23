7642cf70ccc0ede23dc272070cf8aa362b2415a8
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-01-23 12:45:57 -0300

    Update aux_2.sql

--- procs_sobreposicao/aux_2.sql
+++ procs_sobreposicao/aux_2.sql
@@ -14,20 +14,6 @@ ON ST_Intersects(a.valid_geom, b.valid_geom) AND a.gid <> b.gid AND a.gid < b.gi
 
 
 
--- Fronteira
-\echo Faixa de fronteira
-\echo  
-\echo 
-
-
-INSERT INTO autointersection.autointersection_input_faixa_fronteira
-SELECT ROW_NUMBER() OVER () gid, a.gid agid, b.gid bgid, (ST_Dump(ST_CollectionExtract(ST_Intersection(a.valid_geom, b.valid_geom), 3))).geom geom
-FROM dados_brutos.valid_input_faixa_fronteira a
-JOIN dados_brutos.valid_input_faixa_fronteira b 
-ON ST_Intersects(a.valid_geom, b.valid_geom) AND a.gid <> b.gid AND a.gid < b.gid WHERE (a.gid % :var_num_proc) = :var_proc;  ;
-
-
-
 -- SICAR
 \echo SICAR
 \echo  
