7a782937c1c5b13f3da3ce47129ce543469a0714
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-01-23 12:35:07 -0300

    Create bash_execproc copy 2.sh

---
+++ procs_sobreposicao/bash_execproc copy 2.sh
@@ -0,0 +1,54 @@
+userName=postgres
+databaseName=malha_fundiaria
+numProc=120
+export PGPASSWORD='gpp-es@lq'
+
+
+psql -U $userName -d $databaseName -f ${procName}_1.sql
+
+
+#If sql 2 exists execute it
+for ((i=0; i < ${numProc}; i++))
+do
+    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_2.sql &
+done > log_autointersection 2>&1
+wait
+
+
+#If sql 2 exists execute it
+for ((i=0; i < ${numProc}; i++))
+do
+    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_3.sql &
+done > log_inputs 2>&1
+wait
+
+
+procName=proc5_overlay
+
+#If sql 2 exists execute it
+for ((i=0; i < ${numProc}; i++))
+do
+    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f procs/${procName}_2_17.sql &
+done > log_massas 2>&1
+wait
+
+#If sql 2 exists execute it
+for ((i=0; i < ${numProc}; i++))
+do
+    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f procs/${procName}_2_18.sql &
+done > log_fronteira 2>&1
+wait
+
+#If sql 2 exists execute it
+for ((i=0; i < ${numProc}; i++))
+do
+    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f procs/${procName}_2_19.sql &
+done > log_sicar 2>&1
+wait
+
+
+
+echo Terminou
+
+
+
