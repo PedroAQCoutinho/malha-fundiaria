a202b7b10513df39e2c31119b418c039a5c4fe36
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-01-26 15:47:31 -0300

    Update rasterize.sh

--- auxiliar/rasterize.sh
+++ auxiliar/rasterize.sh
@@ -3,5 +3,5 @@
 export PGPASSWORD="gpp-es@lq"
 
 
-gdal_rasterize  -a gid -of GTiff -a_srs EPSG:4326 -co "COMPRESS=DEFLATE" -co "PREDICTOR=2" -co "ZLEVEL=9" -co "BIGTIFF=IF_SAFER" -co "NUM_THREADS=8" -co "TILED=YES" -te -74.8973961 -34.7917510 -33.0613262 7.9120915 -ts 155239 158459 -ot UInt32 PG:"user=postgres dbname=malha_fundiaria port=5433" -sql "select gid, valid_geom geom from dados_brutos.valid_sicar_imovel" $HOME/sicar_imovel.tif
-gdal_rasterize  -a gid -of GTiff -a_srs EPSG:4326 -co "COMPRESS=DEFLATE" -co "PREDICTOR=2" -co "ZLEVEL=9" -co "BIGTIFF=IF_SAFER" -co "NUM_THREADS=8" -co "TILED=YES" -te -74.8973961 -34.7917510 -33.0613262 7.9120915 -ts 155239 158459 -ot UInt32 PG:"user=postgres dbname=malha_fundiaria port=5433" -sql "select gid, geom from outputs.step14_overlay" $HOME/step14_overlay.tif
+#gdal_rasterize  -a gid -of GTiff -a_srs EPSG:4326 -co "COMPRESS=DEFLATE" -co "PREDICTOR=2" -co "ZLEVEL=9" -co "BIGTIFF=IF_SAFER" -co "NUM_THREADS=8" -co "TILED=YES" -te -74.8973961 -34.7917510 -33.0613262 7.9120915 -ts 155239 158459 -ot UInt32 PG:"user=postgres dbname=malha_fundiaria port=5433" -sql "select gid, valid_geom geom from dados_brutos.valid_sicar_imovel" $HOME/sicar_imovel.tif
+gdal_rasterize  -a gid -of GTiff -a_srs EPSG:4326 -co "COMPRESS=DEFLATE" -co "PREDICTOR=2" -co "ZLEVEL=9" -co "BIGTIFF=IF_SAFER" -co "NUM_THREADS=8" -co "TILED=YES" -te -74.8973961 -34.7917510 -33.0613262 7.9120915 -ts 155239 158459 -ot UInt32 PG:"user=postgres dbname=malha_fundiaria port=5433" -sql "select gid, geom from outputs.step15_overlay" $HOME/step15_overlay.tif
