5a0068b44cad36776790aad339de9e85995126d3
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-01-27 15:22:34 -0300

    Create proc2.R

---
+++ procs_area/proc2.R
@@ -0,0 +1,52 @@
+rm(list=ls())
+options(scipen = 999)
+options(stringsAsFactors = F)
+# installing packages
+library(pacman)
+p_load(data.table, raster, rgdal, sf, fasterize, dplyr,exactextractr, doMPI, snow, raster, RPostgreSQL)
+source('procs_area/funcoes.R')
+
+tryCatch({
+  drv <- dbDriver("PostgreSQL")
+  print("Connecting to Database…")
+  connec <- dbConnect(drv, 
+                      dbname = 'malha_fundiaria',
+                      host = '34.71.4.88', 
+                      port = '5433',
+                      user = 'postgres', 
+                      password = 'gpp-es@lq')
+  print("Database Connected!")
+},
+error=function(cond) {
+  print("Unable to connect to Database.")
+})
+
+dbListTables(connec)[order(dbListTables(connec))]
+
+#dados
+desmatamento <- raster('/home/pedro/hd1/dados_GPP/uso_terra/desmatamento/PRODES/desmatamento_prodes_2000_2021_4326.tif')
+car <- raster('/home/pedro/hd1/pedro/GPP/ltmodel/outputs/geotiffs/sicar_imovel.tif')
+cat_fund <- raster('/home/pedro/hd1/pedro/GPP/ltmodel/outputs/geotiffs/step15_overlay.tif')
+id_cat_fund <- dbGetQuery(connec, "select * from layer_fundiario.step15_id_label")
+bss <- blockSize(desmatamento, minrows = 10) ; bss
+output <- vector('list', bss$n)
+
+
+#Cluster start
+cl <- snow::makeSOCKcluster(14)
+registerDoMPI(cl)
+#Load libraries
+snow::clusterEvalQ(cl, library(raster))
+snow::clusterEvalQ(cl, library(dplyr))
+snow::clusterExport(cl=cl,
+                    ls())
+
+atualiza_dt()
+
+
+
+output <- output %>% left_join(id_cat_fund, by = c('cat_fund' = 'gid')) %>%
+  group_by(cat_fund, car, desmatamento, original_layer_label) %>%
+  summarise(count = sum(count, na.rm = T))
+saveRDS(output, '../../outputs/areas/proc1_area.rds')
+snow::stopCluster(cl)
