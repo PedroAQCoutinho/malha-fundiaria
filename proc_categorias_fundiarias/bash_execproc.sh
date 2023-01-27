ccffbc8b2c05316f7a4b16a2ece503e68349c634
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-01-27 11:35:13 -0300

    Update bash_execproc.sh

--- proc_categorias_fundiarias/bash_execproc.sh
+++ proc_categorias_fundiarias/bash_execproc.sh
@@ -1,17 +1,17 @@
 userName=postgres
 databaseName=malha_fundiaria
 numProc=14
-procName=aux
+procName=proc1_limpeza_duplicados
 
 psql -U $userName -d $databaseName -f ${procName}_1.sql
 
 
 #If sql 2 exists execute it
-for ((i=0; i < ${numProc}; i++))
-do
-    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_2.sql &
-done
-wait
+#for ((i=0; i < ${numProc}; i++))
+#do
+#    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_2.sql &
+#done
+#wait
 
 
 
