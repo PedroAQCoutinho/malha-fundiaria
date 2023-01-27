498c3b34dbb1a72682248fea17febe49ccf988be
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-01-27 15:22:41 -0300

    Update aux_2.sql

--- procs_sobreposicao/aux_2.sql
+++ procs_sobreposicao/aux_2.sql
@@ -1,5 +1,5 @@
 
-/*
+
 -- Massas dagua
 \echo Massas dagua
 \echo  
@@ -11,8 +11,10 @@ SELECT ROW_NUMBER() OVER () gid, a.gid agid, b.gid bgid, (ST_Dump(ST_CollectionE
 FROM dados_brutos.valid_input_massas_dagua a
 JOIN dados_brutos.valid_input_massas_dagua b 
 ON ST_Intersects(a.valid_geom, b.valid_geom) AND a.gid <> b.gid AND a.gid < b.gid WHERE (a.gid % :var_num_proc) = :var_proc;  ;
-*/
 
+
+
+/*
 
 -- SICAR
 \echo SICAR
@@ -26,3 +28,4 @@ FROM dados_brutos.valid_sicar_imovel a
 JOIN dados_brutos.valid_sicar_imovel b 
 ON ST_Intersects(a.valid_geom, b.valid_geom) AND a.gid <> b.gid AND a.gid < b.gid WHERE (a.gid % :var_num_proc) = :var_proc;  ;
 
+*/
