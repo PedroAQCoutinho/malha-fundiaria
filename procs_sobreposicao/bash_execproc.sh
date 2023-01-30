085b9345ee060a1632fac815f8fa4d1165c1b091
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-01-30 19:19:15 -0300

    Update bash_execproc.sh

--- procs_sobreposicao/bash_execproc.sh
+++ procs_sobreposicao/bash_execproc.sh
@@ -19,20 +19,20 @@ wait
 
 
 #If sql 2 exists execute it
-for ((i=0; i < ${numProc}; i++))
-do
-    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_3.sql &
-done > log_inputs 2>&1
-wait
+#for ((i=0; i < ${numProc}; i++))
+#do
+#    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_3.sql &
+#done > log_inputs 2>&1
+#wait
 
 
 #procName=proc5_overlay
 
 #If sql 2 exists execute it
-for ((i=0; i < ${numProc}; i++))
-do
-    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f procs/${procName}_2_17.sql &
-done > log_massas 2>&1
+#for ((i=0; i < ${numProc}; i++))
+#do
+#    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f procs/${procName}_2_17.sql &
+#done > log_massas 2>&1
 wait
 
 #If sql 2 exists execute it
