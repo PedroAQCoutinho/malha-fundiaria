1d60c87822e4bc04cec45acf9ad0b55e8b701b78
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-01-31 16:38:56 -0300

    Update proc1_reclass_desmatamento.sql

--- irregularidades/proc1_reclass_desmatamento.sql
+++ irregularidades/proc1_reclass_desmatamento.sql
@@ -1,13 +1,14 @@
+DROP TABLE IF EXISTS irregularidades.step14_reclass_desmatamento;
 CREATE TABLE irregularidades.step14_reclass_desmatamento AS 
 SELECT original_layer_label, cat_agrupada, sae.car , am_legal, cd_bioma, cd_mun, CASE 
 	WHEN desmatamento = 7 THEN 5
 	WHEN desmatamento = 40 THEN 7
 	WHEN desmatamento = 30 THEN 6
-	WHEN desmatamento = -1 THEN NULL
+	WHEN desmatamento = -1 THEN 999
 	ELSE desmatamento
 END desmatamento
 FROM layer_fundiario.step14_area_export sae 
-WHERE car != -1
+WHERE car != -1;
 
 
 CREATE INDEX step14_reclass_desmatamento_original_layer_label_idx ON irregularidades.step14_reclass_desmatamento USING btree (original_layer_label);
