f42e0f61faa1fe0d825ac81b0781a57fd7d2a67b
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-02-01 09:58:30 -0300

    Update bash_execproc.sh

--- irregularidades/bash_execproc.sh
+++ irregularidades/bash_execproc.sh
@@ -7,11 +7,11 @@ export PGPASSWORD='gpp-es@lq'
 procName=proc4_car_mun
 
 
-
+psql -U $userName -d $databaseName -f ${procName}_1.sql
 
 #If sql 2 exists execute it
 for ((i=0; i < ${numProc}; i++))
 do
-    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}.sql &
+    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_2.sql &
 done > log_cd_mun 2>&1
 wait
