004cd25a48ddefe06cd7390a6bb064dc04dc15d8
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-02-01 11:03:04 -0300

    Update proc5_tipos_irregulatidade.sql

--- irregularidades/proc5_tipos_irregulatidade.sql
+++ irregularidades/proc5_tipos_irregulatidade.sql
@@ -1,19 +1,5 @@
-WITH foo AS (SELECT gid, flag, original_layer_label, cat_agrupada , area, tipo_imove,
-CASE
-	WHEN flag > 8 AND flag != 999 THEN TRUE
-	ELSE FALSE 
-END is_data_irregular,
-CASE
-	WHEN cat_agrupada IS NOT NULL THEN TRUE
-	ELSE FALSE 
-END is_ocupa_irregular,
-CASE
-	WHEN area > 2500 THEN TRUE
-	ELSE FALSE 
-END is_area_irregular
-FROM dados_brutos.valid_sicar_imovel vsi 
-LEFT JOIN irregularidades.step14_car_flag scf ON gid = scf.car
-LEFT JOIN irregularidades.step14_car_ocupacao sco  ON gid = sco.car)
+DROP TABLE IF EXISTS irregularidades.step14_tipo_oii;
+CREATE TABLE irregularidades.step14_tipo_oii AS 
 SELECT *, 
 CASE 
 	WHEN tipo_imove = 'IRU' AND is_data_irregular AND NOT is_area_irregular AND NOT is_ocupa_irregular THEN 'A'
@@ -24,5 +10,7 @@ CASE
 	WHEN tipo_imove = 'IRU' AND is_data_irregular AND is_area_irregular AND NOT is_ocupa_irregular THEN 'F'
 	WHEN tipo_imove = 'IRU' AND is_data_irregular AND is_area_irregular AND is_ocupa_irregular THEN 'G'
 END tipo
-FROM foo
-WHERE is_area_irregular AND is_ocupa_irregular AND is_data_irregular
+FROM irregularidades.step14_car_join;
+
+
+
