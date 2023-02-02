999a85471efb980cb5f06ca762203dfb7b799908
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-02-02 14:56:52 -0300

    Update proc1_limpeza_duplicados.sql

--- proc_categorias_fundiarias/proc1_limpeza_duplicados.sql
+++ proc_categorias_fundiarias/proc1_limpeza_duplicados.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS layer_fundiario.step14_id_label;
+DROP TABLE IF EXISTS layer_fundiario.step14_id_label_1;
 CREATE TABLE layer_fundiario.step14_id_label AS 
-WITH foo AS (SELECT gid, anyarray_sort(anyarray_uniq(original_layer)) original_layer, am_legal, cd_bioma, cd_mun, ST_Area(geom::geography) area
+WITH foo AS (SELECT gid, anyarray_sort(anyarray_uniq(original_layer)) original_layer, am_legal, cd_bioma, cd_mun, ST_Area(geom::geography) area, geom
 FROM outputs.step14_overlay so ),
 bar AS (SELECT gid, CASE 
 	WHEN original_layer != '{0}' THEN anyarray_uniq(array_remove(
@@ -35,8 +35,8 @@ array_replace(original_layer
 ,'17',7)
 , '16', 1), 0))
 	ELSE original_layer
-END original_layer ,am_legal, cd_bioma, cd_mun, area FROM foo),
-nee AS (SELECT gid, array_sort(original_layer) original_layer_label, original_layer, am_legal, cd_bioma, cd_mun, area FROM bar)
+END original_layer , geom, am_legal, cd_bioma, cd_mun, area FROM foo),
+nee AS (SELECT gid, array_sort(original_layer) original_layer_label, original_layer, am_legal, cd_bioma, cd_mun, area, geom FROM bar)
 SELECT gid, CASE 
 	WHEN original_layer_label != '{0}' THEN 
 	array_replace(
@@ -59,14 +59,5 @@ SELECT gid, CASE
 	, '9', 'MASSA_DAGUA')
 	, '8', 'UCPI')
 	ELSE '{VAZIO}'
-END original_layer_label, am_legal, cd_bioma, cd_mun, area
-FROM nee;
-
-
-
-CREATE INDEX step14_id_label_gid_idx ON layer_fundiario.step14_id_label USING btree (gid);
-CREATE INDEX step14_id_label_original_layer_label_idx ON layer_fundiario.step14_id_label USING btree (original_layer_label);
-
-
-
-
+END original_layer_label, am_legal, cd_bioma, cd_mun, area, geom
+FROM nee WHERE NOT st_isempty(geom) ;
