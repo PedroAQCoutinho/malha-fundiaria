b26c4f905a82e11bb73b75de4e7fc3dcdbb18887
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-02-02 14:57:02 -0300

    Delete proc2_car_flag.sql

--- irregularidades/proc2_car_flag.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE irregularidades.step14_car_flag AS 
-SELECT car, min(desmatamento) flag FROM irregularidades.step14_reclass_desmatamento srd 
-GROUP BY car; 
-
-CREATE INDEX step14_car_flag_cd_mun_idx ON irregularidades.step14_car_flag USING btree (flag);
-CREATE INDEX step14_car_flag_desmatamento_idx ON irregularidades.step14_car_flag USING btree (car);
