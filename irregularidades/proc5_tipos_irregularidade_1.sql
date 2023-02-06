8cab12aa170a1f851f790d1dc5098123cb69c70d
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-02-06 17:26:38 -0300

    Update proc5_tipos_irregularidade_1.sql

--- irregularidades/proc5_tipos_irregularidade_1.sql
+++ irregularidades/proc5_tipos_irregularidade_1.sql
@@ -1,19 +1,22 @@
-DROP TABLE IF EXISTS irregularidades.step14_tipo_oii;
-CREATE TABLE irregularidades.step14_tipo_oii
+DROP TABLE IF EXISTS irregularidades.proc5_step14_tipo_oii;
+CREATE TABLE irregularidades.proc5_step14_tipo_oii
 (
 gid INT NULL , 
-flag INT NULL, 
-original_layer_label TEXT[] NULL , 
-cat_agrupada TEXT[] NULL , 
-area decimal null, 
-tipo_imove TEXT null, 
-is_data_irregular BOOLEAN NULL, 
-is_ocupa_irregular BOOLEAN NULL , 
-is_area_irregular boolean null, 
-tipo TEXT nULL, 
+area_imovel DECIMAL NULL,
+tipo_imove TEXT NULL, 
+uf TEXT NULL,
+cod_munici INT NULL,
+cd_bioma INT NULL, 
+flag int NULL, 
+nm_cat_fund TEXT NULL, 
+nm_agrup TEXT NULL, 
+is_recente BOOLEAN NULL, 
+is_ocupa_irregular BOOLEAN NULL, 
+is_grande BOOLEAN NULL, 
+tipo TEXT NULL,
 geom geometry NULL 
 );
 
 
-CREATE INDEX step14_tipo_oii_gid_idx ON irregularidades.step14_tipo_oii USING btree (gid);
-CREATE INDEX step14_tipo_oii_geom_idx ON irregularidades.step14_tipo_oii USING gist (geom);
+CREATE INDEX proc5_step14_tipo_oii_gid_idx ON irregularidades.proc5_step14_tipo_oii USING btree (gid);
+CREATE INDEX proc5_step14_tipo_oii_geom_idx ON irregularidades.proc5_step14_tipo_oii USING gist (geom);
