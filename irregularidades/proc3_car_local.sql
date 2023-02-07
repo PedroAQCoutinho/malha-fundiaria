d53278ec78dc6fd9fb440835e6e9929f9ba5522c
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-02-07 09:39:24 -0300

    Update proc3_car_local.sql

--- irregularidades/proc3_car_local.sql
+++ irregularidades/proc3_car_local.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS irregularidades.proc32_step14_car_ocupacao_irregular;
 CREATE TABLE irregularidades.proc32_step14_car_ocupacao_irregular as
-SELECT a.gid, a.area, area_desmatamento, area_desmatamento/a.area parea, 
+SELECT a.gid, a.area, area_desmatamento, area_desmatamento/(a.area+0.01) parea, 
 CASE 
-	WHEN area_desmatamento/a.area > 1 THEN FALSE 
-	WHEN area_desmatamento/a.area < 0.05 THEN FALSE 
+	WHEN area_desmatamento/(a.area+0.01) > 1 THEN FALSE 
+	WHEN area_desmatamento/(a.area+0.01) < 0.05 THEN FALSE 
 	ELSE TRUE
 END is_ocupa_irregular,
 original_layer_label, am_legal, cd_bioma, cd_mun, nm_cat_fund, nm_agrup 
@@ -11,8 +11,20 @@ FROM dados_brutos.valid_sicar_imovel a , LATERAL (SELECT *
 FROM  irregularidades.proc2_step14_desmatamento_anual b 
 WHERE b.car = a.gid AND (nm_agrup = 'publica_afetada' 
 OR nm_agrup = 'publica_afetada__imovel_rural_privado' 
-OR nm_agrup = 'publica afetada_coletiva_privada')
-ORDER BY desmatamento LIMIT 1) c
+OR nm_agrup = 'publica afetada_coletiva_privada'
+OR nm_agrup = 'publica imovel_privado_coletivo_publica_destinada')
+ORDER BY desmatamento LIMIT 1) c 
+UNION ALL 
+SELECT a.gid, a.area, area_desmatamento, area_desmatamento/(a.area+0.01)  parea, 
+FALSE is_ocupa_irregular,
+original_layer_label, am_legal, cd_bioma, cd_mun, nm_cat_fund, nm_agrup 
+FROM dados_brutos.valid_sicar_imovel a , LATERAL (SELECT * 
+FROM  irregularidades.proc2_step14_desmatamento_anual b 
+WHERE b.car = a.gid AND (nm_agrup != 'publica_afetada' 
+OR nm_agrup != 'publica_afetada__imovel_rural_privado' 
+OR nm_agrup != 'publica afetada_coletiva_privada'
+OR nm_agrup != 'publica imovel_privado_coletivo_publica_destinada')
+ORDER BY desmatamento LIMIT 1) c 
 
 
 
