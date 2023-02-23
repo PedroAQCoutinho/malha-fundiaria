104d272f5f7fc208569d21da9e5654217583fb0b
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-02-23 17:50:01 -0300

    Update proc1_clustering.sql

--- procs_car/proc1_clustering.sql
+++ procs_car/proc1_clustering.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS car.proc1_clustering ; 
 CREATE TABLE car.proc1_clustering AS 
 SELECT st_asbinary((ST_Dump(ST_Split(ST_Union(g) , ST_Union(g2)))).geom) bn,  (ST_Dump(ST_Split(ST_Union(g), ST_Union(g2)))).geom geom 
 FROM car.valid_sicar_imovel_dump, 
