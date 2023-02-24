156f7401696eee527924af28bde6ea32cabb7de1
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-02-24 15:44:23 -0300

    Update proc6_cat_fund.sql

--- irregularidades/proc6_cat_fund.sql
+++ irregularidades/proc6_cat_fund.sql
@@ -3,33 +3,38 @@ SELECT cat_fund, car, desmatamento , area_desmatamento , am_legal , cd_bioma, cd
 FROM irregularidades.proc2_step14_desmatamento_anual a 
 LEFT JOIN dados_brutos.valid_sicar_imovel vsi ON vsi.gid = a.car 
 
-
-
 CREATE INDEX proc61_step14_cat_fund_car_idx ON irregularidades.proc61_step14_cat_fund USING btree (car);
 CREATE INDEX proc61_step14_cat_fund_cat_fund_idx ON irregularidades.proc61_step14_cat_fund USING btree (cat_fund);
 CREATE INDEX proc61_step14_cat_fund_nm_agrup_idx ON irregularidades.proc61_step14_cat_fund USING btree (nm_agrup);
 
 
+--aux 
+CREATE TABLE irregularidades.proc62_aux AS 
+SELECT car, nm_agrup, sum(area_desmatamento) area_desmatamento, avg(area) area 
+FROM irregularidades.proc61_step14_cat_fund a 
+GROUP BY car, nm_agrup
+
+CREATE INDEX proc62_aux_car_idx ON irregularidades.proc62_aux USING btree (car);
 
 
+-- agrupamento fundiario a que o car pertence (predominante)
 CREATE TABLE irregularidades.proc62_step14_cat_fund AS 
 WITH foo AS (SELECT car, nm_agrup, sum(area_desmatamento) area_desmatamento, avg(area) area 
 FROM irregularidades.proc61_step14_cat_fund a 
 GROUP BY car, nm_agrup)
 SELECT car, nm_agrup , max(area_desmatamento) area_sobrep FROM foo GROUP BY car, nm_agrup
 
-
 CREATE INDEX proc62_step14_cat_fund_car_idx ON irregularidades.proc62_step14_cat_fund USING btree (car);
 CREATE INDEX proc62_step14_cat_fund_nm_agrup_idx ON irregularidades.proc62_step14_cat_fund USING btree (nm_agrup);
 
-
+--caracteristicas de cada car
 
 DROP TABLE IF EXISTS irregularidades.proc63_step14_cat_fund;
 CREATE TABLE irregularidades.proc63_step14_cat_fund AS 
-SELECT gid car, am_legal, cd_bioma, cd_mun, tipo_imove, ST_CollectionExtract(st_force2d(st_makevalid(valid_geom)), 3) geom FROM dados_brutos.valid_sicar_imovel a , LATERAL (
-SELECT am_legal, cd_bioma, cd_mun FROM irregularidades.proc2_step14_desmatamento_anual psda ORDER BY area_desmatamento LIMIT 1
+SELECT gid car, am_legal, cd_bioma, cd_mun, tipo_imove, a.area area_imovel, ST_CollectionExtract(st_force2d(st_makevalid(valid_geom)), 3) geom 
+FROM dados_brutos.valid_sicar_imovel a , LATERAL (
+SELECT car, am_legal, cd_bioma, cd_mun FROM irregularidades.proc2_step14_desmatamento_anual psda WHERE psda.car = a.gid ORDER BY area_desmatamento LIMIT 1
 ) foo
 
-
 CREATE INDEX proc63_step14_cat_fund_car_idx ON irregularidades.proc63_step14_cat_fund USING btree (car);
-CREATE INDEX proc63_step14_cat_fund_geom_idx ON irregularidades.proc63_step14_cat_fund USING btree (geom);
+CREATE INDEX proc63_step14_cat_fund_geom_idx ON irregularidades.proc63_step14_cat_fund USING gist (geom);
