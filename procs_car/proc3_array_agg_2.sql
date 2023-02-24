900260af960bd0a23614d83d1021370ac7d1a7bc
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-02-24 17:11:01 -0300

    Update proc3_array_agg_2.sql

--- procs_car/proc3_array_agg_2.sql
+++ procs_car/proc3_array_agg_2.sql
@@ -1,14 +1,6 @@
-DROP TABLE IF EXISTS car.proc1_clustering ; 
-CREATE TABLE car.proc1_clustering AS 
-SELECT st_asbinary((ST_Dump(ST_Split(ST_Union(g) , ST_Union(g2)))).geom) bin,  --pega o binário de cada feição para group by posterior
-(ST_Dump(ST_Split(ST_Union(g), ST_Union(g2)))).geom geom --feições
-FROM car.valid_sicar_imovel_dump, 
-LATERAL ( --lateral join que retorna as feições clusterizadas. O gid se perde porém será resgatado no proc2 
-SELECT  unnest(ST_ClusterIntersecting(geom)) g , --retorna geometria
-ST_ExteriorRing((ST_Dump(unnest(ST_ClusterIntersecting(geom)))).geom) g2 -- retorna lines para split   
-FROM car.valid_sicar_imovel_dump) bar; 
-
-CREATE INDEX proc1_clustering_bin_idx ON car.proc1_clustering USING btree (bin);
-CREATE INDEX proc1_clustering_geom_idx ON car.proc1_clustering USING GIST (geom);
-
-
+\car_dump :var_proc
+select car_dump(:var_proc)
+\split polygons :var_proc
+select split_polygons(:var_proc)
+\insert_array_agg :var_proc
+select inserto_array_agg(:var_proc)
