06fbe68d8e62479e00bda1002db337719e01802c
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-02-24 17:13:39 -0300

    Update bash_execproc.sh

--- procs_car/bash_execproc.sh
+++ procs_car/bash_execproc.sh
@@ -2,36 +2,22 @@
 
 userName=postgres
 databaseName=malha_fundiaria
-numProc=14
-procName=proc1_overlay
+numProc=1600144
+procName=proc3_array_agg
 
 
-psql -U $userName -d $databaseName -f ${procName}_1.sql
+
+psql -U $userName -d $databaseName -f ${procName}_1.sql &
 
 
 #If sql 2 exists execute it
-for ((i=0; i < ${numProc}; i++))
+for ((i=36209; i < ${numProc}; i++))
 do
-    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_2.sql &
-done
-wait
-
-#If sql 2 exists execute it
-for ((i=0; i < ${numProc}; i++))
-do
-    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_3.sql &
-done
-wait
-
-#If sql 2 exists execute it
-for ((i=0; i < ${numProc}; i++))
-do
-    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_4.sql &
-done
+    psql -U $userName -d $databaseName -v var_proc=$i -f ${procName}_2.sql &
+done 
 wait
 
 
-echo Terminou
 
 
 
