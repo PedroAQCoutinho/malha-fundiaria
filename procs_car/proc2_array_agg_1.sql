bf0cc29ab05a254c76a96d8a0d0bac1ee8de4ab3
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-02-27 15:45:04 -0300

    Update proc2_array_agg_1.sql

--- procs_car/proc2_array_agg_1.sql
+++ procs_car/proc2_array_agg_1.sql
@@ -1,7 +1,11 @@
+
 DROP TABLE IF EXISTS car.proc2_array_agg;
-CREATE TABLE car.proc2_array_agg (
+CREATE TABLE IF NOT EXISTS car.proc2_array_agg (
 
 gid int[] null,
+cd_grid integer null, 
+cd_mun integer null, 
+am_legal boolean null ,
 area decimal null,
 geom geometry null
 
@@ -12,17 +16,18 @@ CREATE INDEX proc2_array_agg_bin_idx ON car.proc2_array_agg USING btree (gid);
 CREATE INDEX proc2_array_agg_geom_idx ON car.proc2_array_agg USING GIST (geom);
 
 
-CREATE SEQUENCE IF NOT EXISTS car.car_break_seq INCREMENT BY 1 MINVALUE 36200 MAXVALUE 1600144 START WITH 36200;
+CREATE SEQUENCE IF NOT EXISTS car.car_break_seq INCREMENT BY 1 MINVALUE 880122 MAXVALUE 1600144 START WITH 880122;
 
 CREATE OR REPLACE FUNCTION car_dump(t_name varchar(30))
   RETURNS VOID
   LANGUAGE plpgsql AS
 $func$
 BEGIN
-   EXECUTE format('CREATE TABLE car_dump_%s AS
-      SELECT a.gid, st_collectionextract((ST_Dump(ST_Intersection(a.valid_geom, b.geom))).geom, 3) geom 
-      FROM dados_brutos.valid_sicar_imovel a , temporario.adm2_overlay b
-      WHERE ST_Intersects(a.valid_geom, b.geom) AND b.cd_grid = cast(%s as integer);
+   EXECUTE format('CREATE TEMPORARY TABLE car_dump_%s AS
+      SELECT a.gid, cd_grid, cd_mun, am_legal, 
+      st_collectionextract((ST_Dump(ST_Intersection(a.valid_geom, b.geom))).geom, 3) geom 
+      FROM dados_brutos.valid_sicar_imovel a LEFT JOIN temporario.adm2_overlay b
+      ON ST_Intersects(a.valid_geom, b.geom) WHERE  b.cd_grid = cast(%s as integer);
       CREATE INDEX car_dump_%s_gid_idx ON car_dump_%s USING btree (gid);
       CREATE INDEX car_dump_%s_geom_idx ON car_dump_%s USING GIST (geom);', t_name, t_name, t_name, t_name, t_name, t_name);
 END
@@ -38,7 +43,7 @@ CREATE OR REPLACE FUNCTION split_polygons(t_name varchar(30))
 $func$
 BEGIN
    EXECUTE format('
-      CREATE TABLE proc1_split_polygons_%s AS 
+      CREATE TEMPORARY TABLE proc1_split_polygons_%s AS 
       SELECT st_asbinary((ST_Dump(ST_Split(ST_Union(geom), ST_Union(st_exteriorring(geom))))).geom) bin, 
       (ST_Dump(ST_Split(ST_Union(geom), ST_Union(st_exteriorring(geom))))).geom 
       FROM car_dump_%s;
@@ -57,12 +62,12 @@ CREATE OR REPLACE FUNCTION inserto_array_agg(t_name varchar(30))
 $func$
 BEGIN
    EXECUTE format('
-      INSERT INTO car.proc2_array_agg (geom, gid, area) 
+      INSERT INTO car.proc2_array_agg (geom, gid, cd_grid, cd_mun, am_legal, area) 
       SELECT ST_Union(ST_Force2d(a.geom)) geom,
-      array_agg(b.gid) gid,
+      array_agg(b.gid) gid, cd_grid, cd_mun, am_legal,
       ST_Area(ST_Union(ST_Force2d(a.geom))::geography) area  FROM proc1_split_polygons_%s a 
       LEFT JOIN car_dump_%s b ON ST_Intersects(ST_Buffer(a.geom::geography, -5), b.geom) 
-      GROUP BY bin;', t_name, t_name);
+      GROUP BY bin, cd_grid, cd_mun, am_legal;', t_name, t_name);
 END
 $func$;
 
