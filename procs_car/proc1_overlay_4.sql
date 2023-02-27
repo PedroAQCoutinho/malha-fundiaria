c221fb0d1639781873c102804ee674554a579743
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-02-27 11:10:49 -0300

    Update proc1_overlay_4.sql

--- procs_car/proc1_overlay_4.sql
+++ procs_car/proc1_overlay_4.sql
@@ -11,7 +11,9 @@ SELECT * FROM (SELECT
 		WHEN ST_Intersects(a.geom, b.geom) AND NOT ST_Within(a.geom, b.geom) THEN ST_CollectionExtract(ST_Intersection(a.geom,  b.geom), 3) 
 	END geom
 FROM 
-	temporario.adm2_overlay a , geo_adm.pa_br_limitebiomas_250_2006_ibge_4674 b
+	temporario.adm2_overlay a 
+LEFT JOIN 
+	geo_adm.pa_br_limitebiomas_250_2006_ibge_4674 b ON ST_Intersects(a.geom, b.geom)
 WHERE 
 	(a.gid % :var_num_proc) = :var_proc	
 UNION ALL 
@@ -25,9 +27,16 @@ SELECT
 		WHEN ST_Intersects(a.geom, b.geom) AND NOT ST_Within(a.geom, b.geom) THEN ST_CollectionExtract(ST_Difference(a.geom,  b.geom), 3) 
 	END geom
 FROM 
-	temporario.adm2_overlay a , geo_adm.pa_br_limitebiomas_250_2006_ibge_4674 b
+	temporario.adm2_overlay a 
+LEFT JOIN 
+	geo_adm.pa_br_limitebiomas_250_2006_ibge_4674 b ON ST_Intersects(a.geom, b.geom)
 WHERE 
 	(a.gid % :var_num_proc) = :var_proc) as foo WHERE geom IS NOT NULL ;
+	
+
+
+
+
 
 
 
