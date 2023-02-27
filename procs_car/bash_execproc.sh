f941b5ad9a7fd0dcc70c7c3a526ac71bc9f9881b
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-02-27 16:49:41 -0300

    Update bash_execproc.sh

--- procs_car/bash_execproc.sh
+++ procs_car/bash_execproc.sh
@@ -8,6 +8,20 @@ procName=proc1_overlay
 export PGPASSWORD='gpp-es@lq'
 
 
+function displaytime {
+  local T=$1
+  local D=$((T/60/60/24))
+  local H=$((T/60/60%24))
+  local M=$((T/60%60))
+  local S=$((T%60))
+  (( $D > 0 )) && printf '%d days ' $D
+  (( $H > 0 )) && printf '%d hours ' $H
+  (( $M > 0 )) && printf '%d minutes ' $M
+  (( $D > 0 || $H > 0 || $M > 0 )) && printf 'and '
+  printf '%d seconds\n' $S
+}
+
+
 psql -U $userName -d $databaseName -f ${procName}_1.sql
 
 
