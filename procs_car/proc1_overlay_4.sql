5b1cefc1db730b5d80fc353ed2e859b2d4d78c0e
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-02-27 16:20:05 -0300

    Update proc1_overlay_4.sql

--- procs_car/proc1_overlay_4.sql
+++ procs_car/proc1_overlay_4.sql
@@ -1,6 +1,6 @@
 \echo adm3overlay
 
-INSERT INTO temporario.adm3_overlay ( cd_mun , cd_grid, am_legal, cd_bioma,  geom)
+INSERT INTO grid.adm3_overlay ( cd_mun , cd_grid, am_legal, cd_bioma,  geom)
 SELECT * FROM (SELECT
 	cd_mun::int,
 	a.cd_grid::int,
@@ -11,7 +11,7 @@ SELECT * FROM (SELECT
 		WHEN ST_Intersects(a.geom, b.geom) AND NOT ST_Within(a.geom, b.geom) THEN ST_CollectionExtract(ST_Intersection(a.geom,  b.geom), 3) 
 	END geom
 FROM 
-	temporario.adm2_overlay a 
+	grid.adm2_overlay a 
 LEFT JOIN 
 	geo_adm.pa_br_limitebiomas_250_2006_ibge_4674 b ON ST_Intersects(a.geom, b.geom)
 WHERE 
@@ -27,7 +27,7 @@ SELECT
 		WHEN ST_Intersects(a.geom, b.geom) AND NOT ST_Within(a.geom, b.geom) THEN ST_CollectionExtract(ST_Difference(a.geom,  b.geom), 3) 
 	END geom
 FROM 
-	temporario.adm2_overlay a 
+	grid.adm2_overlay a 
 LEFT JOIN 
 	geo_adm.pa_br_limitebiomas_250_2006_ibge_4674 b ON ST_Intersects(a.geom, b.geom)
 WHERE 
