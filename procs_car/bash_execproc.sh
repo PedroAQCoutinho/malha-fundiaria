a2ccf80482c08eca01697357c4455ce3297a6dce
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-02-27 16:20:18 -0300

    Update bash_execproc.sh

--- procs_car/bash_execproc.sh
+++ procs_car/bash_execproc.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-
-curDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 start_date="$(date)"
 SECONDS=0
-. $curDir/bash_01_helpers.sh
-
-
 userName=postgres
 databaseName=malha_fundiaria
 numProc=120
@@ -37,6 +32,8 @@ do
     psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_4.sql &
 done 
 wait
+
+
 
 
 
