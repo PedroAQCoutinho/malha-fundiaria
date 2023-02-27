1e5223d4b68bf1f2839fd907f10e3a55e59563e1
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-02-27 15:59:41 -0300

    Update proc2_array_agg_2.sql

--- procs_car/proc2_array_agg_2.sql
+++ procs_car/proc2_array_agg_2.sql
@@ -1,7 +1,7 @@
 
 \echo RUN :run
-select car_dump(nextval('car.car_break_seq')::text);
-select split_polygons(currval('car.car_break_seq')::text);
-select inserto_array_agg(currval('car.car_break_seq')::text);
+select car_dump(:run::text);
+select split_polygons(:run::text);
+select inserto_array_agg(:run::text);
 
 
