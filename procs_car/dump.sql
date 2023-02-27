fbdc2afe9a606e63b4496efce75248d77ba0f767
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-02-27 08:41:07 -0300

    Delete dump.sql

--- procs_car/dump.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE car.valid_sicar_imovel_dump AS 
-SELECT gid, (st_dump(valid_geom)).geom geom FROM dados_brutos.valid_sicar_imovel vsi 
-
-
-CREATE INDEX valid_sicar_imovel_dump_gid_idx ON car.valid_sicar_imovel_dump USING btree (gid);
-CREATE INDEX valid_sicar_imovel_dump_geom_idx ON car.valid_sicar_imovel_dump USING GIST (geom);
