6bc17a56731f12297dc756ccc6b936c05fa4a27f
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-02-27 16:28:54 -0300

    Update bash_runAll.sh

--- procs_car/bash_runAll.sh
+++ procs_car/bash_runAll.sh
@@ -1,2 +1,3 @@
-bash bash_execproc.sh > log_overlay 2>&1 &
-bash bash_queue_run.sh > log_split 2>&1 &
+bash bash_execproc.sh > log_overlay 2>&1 
+bash bash_queue_run.sh > log_split 2>&1 
+export PGPASSWORD='gpp-es@lq'
