9822fcec2d75e6365e05486d54508da14f561091
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-02-27 08:41:22 -0300

    Update bash_execproc.sh

--- procs_car/bash_execproc.sh
+++ procs_car/bash_execproc.sh
@@ -2,22 +2,16 @@
 
 userName=postgres
 databaseName=malha_fundiaria
-numProc=1600144
-procName=proc3_array_agg
+numProc=10
+procName=proc2_array_agg
 
 
-
-psql -U $userName -d $databaseName -f ${procName}_1.sql &
+psql -U $userName -d $databaseName -f ${procName}_1.sql
 
 
 #If sql 2 exists execute it
-for ((i=36209; i < ${numProc}; i++))
+for ((i=0; i < ${numProc}; i++))
 do
     psql -U $userName -d $databaseName -v var_proc=$i -f ${procName}_2.sql &
 done 
 wait
-
-
-
-
-
