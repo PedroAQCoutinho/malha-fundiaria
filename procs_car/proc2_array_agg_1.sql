346f0e97759e035629e38f29899b4baee7f3e76a
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-02-27 16:20:03 -0300

    Update proc2_array_agg_1.sql

--- procs_car/proc2_array_agg_1.sql
+++ procs_car/proc2_array_agg_1.sql
@@ -6,6 +6,7 @@ gid int[] null,
 cd_grid integer null, 
 cd_mun integer null, 
 am_legal boolean null ,
+cd_bioma int null, 
 area decimal null,
 geom geometry null
 
@@ -16,15 +17,13 @@ CREATE INDEX proc2_array_agg_bin_idx ON car.proc2_array_agg USING btree (gid);
 CREATE INDEX proc2_array_agg_geom_idx ON car.proc2_array_agg USING GIST (geom);
 
 
-CREATE SEQUENCE IF NOT EXISTS car.car_break_seq INCREMENT BY 1 MINVALUE 40200 MAXVALUE 1600144 START WITH 40200;
-
 CREATE OR REPLACE FUNCTION car_dump(t_name varchar(30))
   RETURNS VOID
   LANGUAGE plpgsql AS
 $func$
 BEGIN
    EXECUTE format('CREATE TEMPORARY TABLE car_dump_%s AS
-      SELECT a.gid, cd_grid, cd_mun, am_legal, 
+      SELECT a.gid, cd_grid, cd_mun, am_legal, cd_bioma
       st_collectionextract((ST_Dump(ST_Intersection(a.valid_geom, b.geom))).geom, 3) geom 
       FROM dados_brutos.valid_sicar_imovel a LEFT JOIN temporario.adm2_overlay b
       ON ST_Intersects(a.valid_geom, b.geom) WHERE  b.cd_grid = cast(%s as integer);
@@ -62,9 +61,9 @@ CREATE OR REPLACE FUNCTION inserto_array_agg(t_name varchar(30))
 $func$
 BEGIN
    EXECUTE format('
-      INSERT INTO car.proc2_array_agg (geom, gid, cd_grid, cd_mun, am_legal, area) 
+      INSERT INTO car.proc2_array_agg (geom, gid, cd_grid, cd_mun, am_legal, cd_bioma, area) 
       SELECT ST_Union(ST_Force2d(a.geom)) geom,
-      array_agg(b.gid) gid, cd_grid, cd_mun, am_legal,
+      array_agg(b.gid) gid, cd_grid, cd_mun, am_legal, cd_bioma
       ST_Area(ST_Union(ST_Force2d(a.geom))::geography) area  FROM proc1_split_polygons_%s a 
       LEFT JOIN car_dump_%s b ON ST_Intersects(ST_Buffer(a.geom::geography, -5), b.geom) 
       GROUP BY bin, cd_grid, cd_mun, am_legal;', t_name, t_name);
