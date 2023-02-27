150bf5fa2b945f23aa974d0f923679e89414ed52
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-02-27 15:45:10 -0300

    Update bash_execproc.sh

--- procs_car/bash_execproc.sh
+++ procs_car/bash_execproc.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+curDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+start_date="$(date)"
+SECONDS=0
+. $curDir/bash_01_helpers.sh
+
+
 userName=postgres
 databaseName=malha_fundiaria
-numProc=10
+numProc=120
 procName=proc1_overlay
 
+
+psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_1.sql &
+
+
+#If sql 2 exists execute it
+for ((i=0; i < ${numProc}; i++))
+do
+    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_2.sql &
+done 
+wait
 
 
 
@@ -21,3 +37,6 @@ do
     psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_4.sql &
 done 
 wait
+
+
+
