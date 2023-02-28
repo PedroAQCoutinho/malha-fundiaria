20fbfd9adb1009d0620939f7a7624e01b8ddb0e8
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-02-28 08:50:46 -0300

    Update bash_execproc.sh

--- procs_car/bash_execproc.sh
+++ procs_car/bash_execproc.sh
@@ -22,23 +22,23 @@ function displaytime {
 }
 
 
-psql -U $userName -d $databaseName -f ${procName}_1.sql
-
-
-#If sql 2 exists execute it
-for ((i=0; i < ${numProc}; i++))
-do
-    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_2.sql &
-done 
-wait
-
-
-#If sql 2 exists execute it
-for ((i=0; i < ${numProc}; i++))
-do
-    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_3.sql &
-done 
-wait
+#psql -U $userName -d $databaseName -f ${procName}_1.sql
+#
+#
+##If sql 2 exists execute it
+#for ((i=0; i < ${numProc}; i++))
+#do
+#    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_2.sql &
+#done 
+#wait
+#
+#
+##If sql 2 exists execute it
+#for ((i=0; i < ${numProc}; i++))
+#do
+#    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_3.sql &
+#done 
+#wait
 
 #If sql 2 exists execute it
 for ((i=0; i < ${numProc}; i++))
