f7f18597a488b37d20cdd3f99b3489501c56bf84
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-03-02 11:21:36 -0300

    Update proc2_desmatamento_anual_2.sql

--- irregularidades/proc2_desmatamento_anual_2.sql
+++ irregularidades/proc2_desmatamento_anual_2.sql
@@ -2,13 +2,7 @@
 
 INSERT INTO irregularidades.proc2_step14_desmatamento_anual (cat_fund, car, desmatamento, area_desmatamento, original_layer_label, 
 am_legal, cd_bioma, cd_mun, area_categoria_fundiaria, nm_cat_fund, nm_agrup ) 
-SELECT cat_fund::int, car, CASE 
-	WHEN desmatamento = 7 THEN 5
-	WHEN desmatamento = 40 THEN 7
-	WHEN desmatamento = 30 THEN 6
-	WHEN desmatamento = -1 THEN 999
-	ELSE desmatamento
-END desmatamento , count*0.0875 area_desmatamento , original_layer_label , am_legal, cd_bioma, cd_mun, area area_categoria_fundiaria,
+SELECT cat_fund::int, car, desmatamento , count*0.0875 area_desmatamento , original_layer_label , am_legal, cd_bioma, cd_mun, area area_categoria_fundiaria,
  nm_cat_fund, nm_agrup 
 FROM  layer_fundiario.step14_id_label sil
 LEFT JOIN irregularidades.proc1_step14_area_raw psar  ON gid = cat_fund::int
