24e3c8e58af70c843ca2875bf52328e6c6b65bdf
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-03-02 15:18:57 -0300

    Update proc2_desmatamento_anual_2.sql

--- irregularidades/proc2_desmatamento_anual_2.sql
+++ irregularidades/proc2_desmatamento_anual_2.sql
@@ -1,8 +1,8 @@
 \echo proc2_step14_desmatamento_anual
 
-INSERT INTO irregularidades.proc2_step14_desmatamento_anual (cat_fund, car,  car_gid, desmatamento, area_desmatamento, original_layer_label, 
+INSERT INTO irregularidades.proc2_step14_desmatamento_anual (cat_fund, car_gid, car,   desmatamento, area_desmatamento, original_layer_label, 
 am_legal, cd_bioma, cd_mun, area_categoria_fundiaria, nm_cat_fund, nm_agrup ) 
-SELECT cat_fund::int, c.gid car, c.original_gid car_gid,
+SELECT cat_fund::int, c.gid car_gid, c.original_gid car,
 desmatamento , count*0.0875 area_desmatamento , original_layer_label , sil.am_legal, sil.cd_bioma, sil.cd_mun, sil.area area_categoria_fundiaria,
 nm_cat_fund, nm_agrup 
 FROM  layer_fundiario.step14_id_label sil
