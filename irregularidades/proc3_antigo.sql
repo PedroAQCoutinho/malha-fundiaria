d395efac81b2a7804079143a1d0a9b2425c68091
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-03-03 15:37:44 -0300

    Update proc3_antigo.sql

--- irregularidades/proc3_antigo.sql
+++ irregularidades/proc3_antigo.sql
@@ -1,8 +1,10 @@
 DROP TABLE IF EXISTS irregularidades.proc3_step14_ano_ocupacao;
 CREATE TABLE irregularidades.proc3_step14_ano_ocupacao AS 
-DROP TABLE IF EXISTS irregularidades.proc3_step14_ano_ocupacao;
-CREATE TABLE irregularidades.proc3_step14_ano_ocupacao AS 
-SELECT gid, * FROM dados_brutos.valid_sicar_imovel vsi, LATERAL (
-SELECT  *, CASE WHEN desmatamento > 2009 THEN TRUE ELSE FALSE end is_recente FROM irregularidades.proc2_step14_desmatamento_anual psda WHERE vsi.gid = psda.car ORDER BY desmatamento 
-) foo 
-WHERE (vsi.gid % :var_num_proc) = :var_proc
+SELECT gid, desmatamento, is_recente
+FROM dados_brutos.valid_sicar_imovel a, LATERAL 
+(SELECT *, CASE 
+	WHEN desmatamento > 2009 THEN TRUE 
+	ELSE FALSE 
+END is_recente FROM irregularidades.proc2_step14_desmatamento_anual b 
+WHERE a.gid = b.car AND gid = 379000 ORDER BY desmatamento LIMIT 1) foo
+WHERE (a.gid % :var_num_proc) = :var_proc
