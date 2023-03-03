71e96bc46e4415a3954bb7364df4e02b6181f4c6
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-03-03 15:01:58 -0300

    Update proc4_grande.sql

--- irregularidades/proc4_grande.sql
+++ irregularidades/proc4_grande.sql
@@ -4,6 +4,6 @@ SELECT gid car, area, CASE
 	WHEN area > 2500 THEN TRUE 
 	ELSE false
 END is_grande
-FROM dados_brutos.valid_sicar_imovel vsi 
+FROM dados_brutos.valid_sicar_imovel vsi;
 
 CREATE INDEX proc4_step14_tamanho_ocupacao_car_idx ON irregularidades.proc4_step14_tamanho_ocupacao USING btree (car);
