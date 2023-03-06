f6154d60eee3c35113da72abd991db4bfe62c11a
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-03-06 17:02:06 -0300

    Create proc7_osii_ocii_2.sql

---
+++ irregularidades/proc7_osii_ocii_2.sql
@@ -0,0 +1,40 @@
+
+INSERT INTO irregularidades.proc7_step14_categorizacao (car,  area_imovel, am_legal, cd_mun, cd_bioma, original_layer_label, nm_agrup, nm_cat_fund, is_grande, is_recente, is_local_restrito, tipo_irregularidade, geom )
+SELECT a.gid car, a.area area_imovel, am_legal, cd_mun, cd_bioma, original_layer_label, nm_agrup, nm_cat_fund , is_grande, is_recente , is_local_restrito, CASE 
+	WHEN tipo_imove = 'IRU' AND (nm_agrup <> ' coletiva_privada' or nm_agrup <> 'imovel_rural_privado' or nm_agrup <> 'coletiva_privada_imovel_privado')
+ AND is_recente AND NOT is_grande AND NOT is_local_restrito AND am_legal  THEN 'A'
+	WHEN tipo_imove = 'IRU' AND ( nm_agrup <> ' coletiva_privada' or nm_agrup <> 'imovel_rural_privado' or nm_agrup <> 'coletiva_privada_imovel_privado' )
+ AND NOT is_recente AND NOT is_grande AND is_local_restrito AND am_legal  THEN 'B'
+	WHEN tipo_imove = 'IRU' AND ( nm_agrup <> ' coletiva_privada' or nm_agrup <> 'imovel_rural_privado' or nm_agrup <> 'coletiva_privada_imovel_privado' )
+ AND is_recente AND NOT is_grande AND is_local_restrito AND am_legal  THEN 'C'
+	WHEN tipo_imove = 'IRU' AND ( nm_agrup <> ' coletiva_privada' or nm_agrup <> 'imovel_rural_privado' or nm_agrup <> 'coletiva_privada_imovel_privado' )
+ AND NOT is_recente AND is_grande AND NOT is_local_restrito AND am_legal  THEN 'D'
+	WHEN tipo_imove = 'IRU' AND ( nm_agrup <> ' coletiva_privada' or nm_agrup <> 'imovel_rural_privado' or nm_agrup <> 'coletiva_privada_imovel_privado')
+ AND NOT is_recente AND is_grande AND is_local_restrito AND am_legal  THEN 'E'
+	WHEN tipo_imove = 'IRU' AND ( nm_agrup <> ' coletiva_privada' or nm_agrup <> 'imovel_rural_privado' or nm_agrup <> 'coletiva_privada_imovel_privado' )
+ AND is_recente AND is_grande AND NOT is_local_restrito AND am_legal  THEN 'F'
+	WHEN tipo_imove = 'IRU' AND ( nm_agrup <> ' coletiva_privada' or nm_agrup <> 'imovel_rural_privado' or nm_agrup <> 'coletiva_privada_imovel_privado' )
+ AND is_recente AND is_grande AND is_local_restrito AND am_legal  THEN 'G'
+	ELSE 'OSII'
+END tipo_irregularidade, valid_geom geom
+FROM dados_brutos.valid_sicar_imovel a
+LEFT JOIN irregularidades.proc3_step14_ano_ocupacao b ON a.gid = b.gid
+LEFT JOIN irregularidades.proc4_step14_tamanho_ocupacao c ON a.gid = c.car
+LEFT JOIN irregularidades.proc5_step14_categoria_fundiaria d ON a.gid = d.car
+LEFT JOIN irregularidades.proc6_step14_local_restrito e ON a.gid = e.car
+LEFT JOIN auxiliares.step14_chave_geral f USING(original_layer_label)
+LEFT JOIN auxiliares.step14_chave_agrupamento g USING (id_agrup)
+LEFT JOIN auxiliares.step14_chave_categorias_limpas h USING (id_cat_fund)
+LEFT JOIN LATERAL (
+SELECT car, am_legal, cd_mun, cd_bioma FROM irregularidades.proc2_step14_desmatamento_anual psda WHERE a.gid = psda.car ORDER BY area_desmatamento DESC 
+LIMIT 1) foo
+ON TRUE
+
+
+
+
+
+
+
+
+
