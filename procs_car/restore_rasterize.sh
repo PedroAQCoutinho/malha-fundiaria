d7fe9a8593e7cc1c98b10b1a8c0884b114a4fcbf
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-03-06 09:15:49 -0300

    Delete restore_rasterize.sh

--- procs_car/restore_rasterize.sh
+++
@@ -1,7 +0,0 @@
-export PGPASSWORD="gpp-es@lq"
-
-psql -U postgres -d malha_fundiaria < /home/pedro/hd1/pedro/GPP/ltmodel/outputs/car/proc2_array_agg.sql
-
-export PGPASSWORD="gpp-es@lq"
-
-gdal_rasterize  -a gid -of GTiff -a_srs EPSG:4326 -co "COMPRESS=DEFLATE" -co "PREDICTOR=2" -co "ZLEVEL=9" -co "BIGTIFF=IF_SAFER" -co "TILED=YES" -co "CPL_LOG=gdallog" -te -74.8973961 -34.7917510 -33.0613262 7.9120915 -ts 155239 158459 -ot UInt32 PG:"user=postgres dbname=malha_fundiaria port=5434" -sql "select gid, geom from car.proc2_array_agg" proc2_array_agg.tif
