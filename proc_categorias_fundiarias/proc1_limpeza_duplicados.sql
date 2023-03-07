75ee0cf3a04b71be0e8f0dbf7411b2ab7efae03c
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-03-07 15:49:38 -0300

    Update proc1_limpeza_duplicados.sql

--- proc_categorias_fundiarias/proc1_limpeza_duplicados.sql
+++ proc_categorias_fundiarias/proc1_limpeza_duplicados.sql
@@ -1,63 +1,12 @@
-DROP TABLE IF EXISTS layer_fundiario.step14_id_label_1;
-CREATE TABLE layer_fundiario.step14_id_label AS 
-WITH foo AS (SELECT gid, anyarray_sort(anyarray_uniq(original_layer)) original_layer, am_legal, cd_bioma, cd_mun, ST_Area(geom::geography) area, geom
-FROM outputs.step14_overlay so ),
-bar AS (SELECT gid, CASE 
-	WHEN original_layer != '{0}' THEN anyarray_uniq(array_remove(
-array_replace(
-array_replace(
-array_replace(
-array_replace(
-array_replace(
-array_replace(
-array_replace(
-array_replace(
-array_replace(
-array_replace(
-array_replace(
-array_replace(
-array_replace(	
-array_replace(
-array_replace(original_layer
-, '2', 3)
-, '1', 2)
-, '20', 9)
-, '4', 4)
-,'5',4)
-, '7' ,5)
-, '8', 5)
-,'9', 1)
-,'10', 1)
-, '12', 6)
-, '13', 8)
-, '14', 5)
-,'15',5)
-,'17',7)
-, '16', 1), 0))
-	ELSE original_layer
-END original_layer , geom, am_legal, cd_bioma, cd_mun, area FROM foo),
-nee AS (SELECT gid, array_sort(original_layer) original_layer_label, original_layer, am_legal, cd_bioma, cd_mun, area, geom FROM bar)
-SELECT gid, CASE 
-	WHEN original_layer_label != '{0}' THEN 
-	array_replace(
-	array_replace(
-	array_replace(
-	array_replace(
-	array_replace(
-	array_replace(
-	array_replace(
-	array_replace(
-	array_replace(
-	original_layer_label::TEXT[]
-	, '1', 'IMOVEL_PRIVADO')
-	, '2', 'TI')
-	, '3', 'Qui')
-	, '4', 'ASSENTAMENTO')
-	, '5', 'GLEBAPUB')
-	, '7', 'MILITAR')
-	, '6', 'UCUS')
-	, '9', 'MASSA_DAGUA')
-	, '8', 'UCPI')
-	ELSE '{VAZIO}'
-END original_layer_label, am_legal, cd_bioma, cd_mun, area, geom
-FROM nee WHERE NOT st_isempty(geom) ;
+
+CREATE TABLE layer_fundiario.proc1_limpeza_duplicados as 
+WITH foo AS (SELECT gid, cd_mun, cd_bioma, am_legal, unnest(original_gid) origid, UNNEST(original_layer) orilayer  FROM outputs.step14_overlay a ),
+bar AS (SELECT gid,
+CASE 
+	WHEN orilayer = 12 AND origid IN (SELECT * FROM auxiliares.input_gid_ucus_apa) THEN 'UCUSAPA'
+	ELSE label_agrupado
+END label_agrupado , orilayer FROM foo
+LEFT JOIN auxiliares.inputs b ON  orilayer = id_input  
+ORDER BY gid, orilayer)
+SELECT gid, anyarray_sort(anyarray_uniq(array_agg(orilayer))) orilayer,  anyarray_sort(anyarray_uniq(array_agg(label_agrupado))) orilabel FROM bar
+GROUP BY gid
