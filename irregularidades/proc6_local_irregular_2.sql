3780644d47da65c7ae9638307e69237a9ba54c6f
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-03-07 11:24:07 -0300

    Update proc6_local_irregular_2.sql

--- irregularidades/proc6_local_irregular_2.sql
+++ irregularidades/proc6_local_irregular_2.sql
@@ -16,5 +16,5 @@ FROM irregularidades.proc2_step14_desmatamento_anual b
 WHERE a.gid = b.car AND 
 (nm_agrup <> ' coletiva_privada' or nm_agrup <> 'imovel_rural_privado' or nm_agrup <> 'coletiva_privada_imovel_privado')
 ORDER BY area_desmatamento DESC
-LIMIT 1) foo ON TRUE WHERE and (a.gid % :var_num_proc) = :var_proc
+LIMIT 1) foo ON TRUE WHERE (a.gid % :var_num_proc) = :var_proc
 
