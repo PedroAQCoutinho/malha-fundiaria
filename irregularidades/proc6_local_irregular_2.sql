04df9fd8f4d7ffcaddb1988a1b3f224c9ee85006
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-03-08 11:52:50 -0300

    Update proc6_local_irregular_2.sql

--- irregularidades/proc6_local_irregular_2.sql
+++ irregularidades/proc6_local_irregular_2.sql
@@ -5,17 +5,18 @@
 INSERT INTO irregularidades.proc6_step14_local_restrito
 SELECT  a.gid car , 
 CASE
-	WHEN m_fiscal <= 4 AND area_desmatamento/area > 0.1 THEN TRUE
-	WHEN m_fiscal > 4 AND m_fiscal <= 15 AND area_desmatamento/area > 0.03 THEN TRUE
-	WHEN m_fiscal > 15 AND area_desmatamento/area > 0.01 THEN TRUE
+	WHEN m_fiscal <= 4 AND area_count/area > 0.1 THEN TRUE
+	WHEN m_fiscal > 4 AND m_fiscal <= 15 AND area_count/area > 0.03 THEN TRUE
+	WHEN m_fiscal > 15 AND area_count/area > 0.01 THEN TRUE
 	ELSE FALSE 
-END is_local_restrito, area area_imovel, m_fiscal, cat_fund cat_fund_irregular, original_layer_label original_layer_label_irregular, nm_cat_fund nm_cat_fund_irregular, nm_agrup nm_agrup_irregular
+END is_local_restrito, area area_imovel, m_fiscal, cat_fund cat_fund_irregular, orilabel original_layer_label_irregular, 
+nm_cat_fund nm_cat_fund_irregular, nm_agrup nm_agrup_irregular
 FROM dados_brutos.valid_sicar_imovel a LEFT JOIN LATERAL
 (SELECT *
-FROM irregularidades.proc2_step14_desmatamento_anual b
+FROM irregularidades.proc23_step14_desmatamento_anual b
 WHERE a.gid = b.car AND 
-(nm_agrup = 'publica_afetada' OR nm_agrup = 'publica_afetada__imovel_rural_privado' OR nm_agrup = 'publica afetada_coletiva_privada' OR nm_agrup = 'publica imovel_privado_coletivo_publica_destinada')
-ORDER BY area_desmatamento DESC
+(nm_agrup = 'publica_afetada' OR nm_agrup = 'publica afetada_coletiva_privada')
+ORDER BY area_count DESC
 LIMIT 1) foo ON TRUE WHERE (a.gid % :var_num_proc) = :var_proc
 
 
