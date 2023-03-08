ff33223228be84f13b9c0e41ace009fb5218189f
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-03-08 15:16:49 -0300

    Update proc3_antigo_2.sql

--- irregularidades/proc3_antigo_2.sql
+++ irregularidades/proc3_antigo_2.sql
@@ -1,15 +1,11 @@
-\echo RUN ANTIGO :var_proc
+\echo RUN :var_proc
 
-INSERT INTO irregularidades.proc6_step14_local_restrito
-SELECT  a.gid, 
-CASE
-	WHEN area_count/area > 0.1 THEN TRUE
+INSERT INTO irregularidades.proc3_step14_ano_ocupacao (gid, desmatamento, is_recente)
+SELECT gid, desmatamento, is_recente
+FROM dados_brutos.valid_sicar_imovel a, LATERAL 
+(SELECT *, CASE 
+	WHEN desmatamento > 9 THEN TRUE 
 	ELSE FALSE 
-END is_local_restrito
-FROM dados_brutos.valid_sicar_imovel a LEFT JOIN LATERAL
-(SELECT *
-FROM irregularidades.proc23_step14_desmatamento_anual b
-WHERE a.gid = b.car AND 
-(nm_agrup <> ' coletiva_privada' or nm_agrup <> 'imovel_rural_privado' or nm_agrup <> 'coletiva_privada_imovel_privado')
-ORDER BY area_count DESC
-LIMIT 1) foo ON TRUE
+END is_recente FROM irregularidades.proc2_step14_desmatamento_anual b 
+WHERE a.gid = b.car  ORDER BY desmatamento LIMIT 1) foo
+WHERE (a.gid % :var_num_proc) = :var_proc
