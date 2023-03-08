70682ca3f1a4f36b995cc69d62e287d0cdba471a
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-03-08 15:17:52 -0300

    Update proc3_antigo_2.sql

--- irregularidades/proc3_antigo_2.sql
+++ irregularidades/proc3_antigo_2.sql
@@ -1,11 +1,11 @@
 \echo RUN :var_proc
 
 INSERT INTO irregularidades.proc3_step14_ano_ocupacao (gid, desmatamento, is_recente)
-SELECT gid, desmatamento, is_recente
+SELECT gid, ano, is_recente
 FROM dados_brutos.valid_sicar_imovel a, LATERAL 
 (SELECT *, CASE 
-	WHEN desmatamento > 9 THEN TRUE 
+	WHEN ano > 9 THEN TRUE 
 	ELSE FALSE 
-END is_recente FROM irregularidades.proc2_step14_desmatamento_anual b 
-WHERE a.gid = b.car  ORDER BY desmatamento LIMIT 1) foo
+END is_recente FROM irregularidades.proc23_step14_desmatamento_anual b 
+WHERE a.gid = b.car  ORDER BY ano LIMIT 1) foo
 WHERE (a.gid % :var_num_proc) = :var_proc
