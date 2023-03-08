75fa320accb8437001f96bcdb09aedd4c5918a5d
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-03-08 16:31:19 -0300

    Update proc7_osii_ocii_2.sql

--- irregularidades/proc7_osii_ocii_2.sql
+++ irregularidades/proc7_osii_ocii_2.sql
@@ -1,33 +1,32 @@
 
 \echo RUN :var_proc
-INSERT INTO irregularidades.proc7_step14_categorizacao (car,  area_imovel, am_legal, cd_mun, cd_bioma, original_layer_label, nm_agrup, nm_cat_fund, is_grande, is_recente, desmatamento, is_local_restrito, tipo_irregularidade, geom )
+INSERT INTO irregularidades.proc7_step14_categorizacao (car,  area_imovel, am_legal, cd_mun, cd_bioma, original_layer_label,  d.nm_agrup, nm_cat_fund, is_grande, is_recente, desmatamento, is_local_restrito, tipo_irregularidade, geom )
 SELECT 
 a.gid car, 
 a.area area_imovel, 
 am_legal, 
 cd_mun, 
 cd_bioma, 
-original_layer_label, 
-nm_agrup, 
-nm_cat_fund , 
+ d.nm_agrup, 
+d.nm_cat_fund , 
 is_grande, 
 is_recente , 
 desmatamento,  
 is_local_restrito, 
 CASE 
-	WHEN tipo_imove = 'IRU' AND (nm_agrup <> ' coletiva_privada' or nm_agrup <> 'imovel_rural_privado' or nm_agrup <> 'coletiva_privada_imovel_privado')
+	WHEN tipo_imove = 'IRU' AND ( d.nm_agrup IN ('publica_afetada', 'glebas_publicas', 'vazio', 'publica afetada_coletiva_privada'))
  AND is_recente AND NOT is_grande AND NOT is_local_restrito AND am_legal  THEN 'A'
-	WHEN tipo_imove = 'IRU' AND ( nm_agrup <> ' coletiva_privada' or nm_agrup <> 'imovel_rural_privado' or nm_agrup <> 'coletiva_privada_imovel_privado' )
+	WHEN tipo_imove = 'IRU' AND (  d.nm_agrup IN ('publica_afetada', 'glebas_publicas', 'vazio', 'publica afetada_coletiva_privada') )
  AND NOT is_recente AND NOT is_grande AND is_local_restrito AND am_legal  THEN 'B'
-	WHEN tipo_imove = 'IRU' AND ( nm_agrup <> ' coletiva_privada' or nm_agrup <> 'imovel_rural_privado' or nm_agrup <> 'coletiva_privada_imovel_privado' )
+	WHEN tipo_imove = 'IRU' AND (  d.nm_agrup IN ('publica_afetada', 'glebas_publicas', 'vazio', 'publica afetada_coletiva_privada') )
  AND is_recente AND NOT is_grande AND is_local_restrito AND am_legal  THEN 'C'
-	WHEN tipo_imove = 'IRU' AND ( nm_agrup <> ' coletiva_privada' or nm_agrup <> 'imovel_rural_privado' or nm_agrup <> 'coletiva_privada_imovel_privado' )
+	WHEN tipo_imove = 'IRU' AND (  d.nm_agrup IN ('publica_afetada', 'glebas_publicas', 'vazio', 'publica afetada_coletiva_privada') )
  AND NOT is_recente AND is_grande AND NOT is_local_restrito AND am_legal  THEN 'D'
-	WHEN tipo_imove = 'IRU' AND ( nm_agrup <> ' coletiva_privada' or nm_agrup <> 'imovel_rural_privado' or nm_agrup <> 'coletiva_privada_imovel_privado')
+	WHEN tipo_imove = 'IRU' AND (  d.nm_agrup IN ('publica_afetada', 'glebas_publicas', 'vazio', 'publica afetada_coletiva_privada'))
  AND NOT is_recente AND is_grande AND is_local_restrito AND am_legal  THEN 'E'
-	WHEN tipo_imove = 'IRU' AND ( nm_agrup <> ' coletiva_privada' or nm_agrup <> 'imovel_rural_privado' or nm_agrup <> 'coletiva_privada_imovel_privado' )
+	WHEN tipo_imove = 'IRU' AND (  d.nm_agrup IN ('publica_afetada', 'glebas_publicas', 'vazio', 'publica afetada_coletiva_privada') )
  AND is_recente AND is_grande AND NOT is_local_restrito AND am_legal  THEN 'F'
-	WHEN tipo_imove = 'IRU' AND ( nm_agrup <> ' coletiva_privada' or nm_agrup <> 'imovel_rural_privado' or nm_agrup <> 'coletiva_privada_imovel_privado' )
+	WHEN tipo_imove = 'IRU' AND (  d.nm_agrup IN ('publica_afetada', 'glebas_publicas', 'vazio', 'publica afetada_coletiva_privada'))
  AND is_recente AND is_grande AND is_local_restrito AND am_legal  THEN 'G'
 	ELSE 'OSII'
 END tipo_irregularidade, valid_geom geom
@@ -36,13 +35,10 @@ LEFT JOIN irregularidades.proc3_step14_ano_ocupacao b ON a.gid = b.gid
 LEFT JOIN irregularidades.proc4_step14_tamanho_ocupacao c ON a.gid = c.car
 LEFT JOIN irregularidades.proc5_step14_categoria_fundiaria d ON a.gid = d.car
 LEFT JOIN irregularidades.proc6_step14_local_restrito e ON a.gid = e.car
-LEFT JOIN auxiliares.step14_chave_geral f USING(original_layer_label)
-LEFT JOIN auxiliares.step14_chave_agrupamento g USING (id_agrup)
-LEFT JOIN auxiliares.step14_chave_categorias_limpas h USING (id_cat_fund)
-LEFT JOIN LATERAL (
-SELECT car, am_legal, cd_mun, cd_bioma FROM irregularidades.proc23_step14_desmatamento_anual psda WHERE a.gid = psda.car ORDER BY area_count DESC 
-LIMIT 1) foo
+LEFT JOIN LATERAL (SELECT * FROM irregularidades.proc23_step14_desmatamento_anual h 
+WHERE h.car = a.gid ORDER BY h.area_count DESC LIMIT 1 ) foo
 ON TRUE WHERE (a.gid % :var_num_proc) = :var_proc
+
 
 
 
