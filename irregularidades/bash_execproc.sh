e73ca1fe712972b8d670ece7fcdbb9364082f4ff
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-03-08 12:30:05 -0300

    Update bash_execproc.sh

--- irregularidades/bash_execproc.sh
+++ irregularidades/bash_execproc.sh
@@ -4,7 +4,7 @@ userName=postgres
 databaseName=malha_fundiaria
 numProc=16
 export PGPASSWORD='gpp-es@lq'
-procName=proc3_antigo
+procName=proc7_osii_ocii
 
 
 psql -U $userName -d $databaseName -f ${procName}_1.sql
