c0a00cbae33e07f3d9b6c4d421159bce55472e32
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-03-08 11:25:50 -0300

    Update proc3_antigo_2.sql

--- irregularidades/proc3_antigo_2.sql
+++ irregularidades/proc3_antigo_2.sql
@@ -5,10 +5,10 @@ SELECT  a.gid,
 CASE
 	WHEN area_desmatamento/area > 0.1 THEN TRUE
 	ELSE FALSE 
-END is_local_restrito, *
+END is_local_restrito
 FROM dados_brutos.valid_sicar_imovel a LEFT JOIN LATERAL
 (SELECT *
-FROM irregularidades.proc2_step14_desmatamento_anual b
+FROM irregularidades.proc23_step14_desmatamento_anual b
 WHERE a.gid = b.car AND 
 (nm_agrup <> ' coletiva_privada' or nm_agrup <> 'imovel_rural_privado' or nm_agrup <> 'coletiva_privada_imovel_privado')
 ORDER BY area_desmatamento DESC
