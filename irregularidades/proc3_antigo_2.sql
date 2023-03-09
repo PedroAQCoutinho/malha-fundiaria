8d63b9d5c5f0385ec4fbf6bebff603d522d55e20
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-03-09 10:24:36 -0300

    Update proc3_antigo_2.sql

--- irregularidades/proc3_antigo_2.sql
+++ irregularidades/proc3_antigo_2.sql
@@ -7,5 +7,5 @@ FROM dados_brutos.valid_sicar_imovel a, LATERAL
 	WHEN ano > 9 THEN TRUE 
 	ELSE FALSE 
 END is_recente FROM irregularidades.proc23_step14_desmatamento_anual b 
-WHERE a.gid = b.car  ORDER BY ano LIMIT 1) foo
+WHERE a.gid = b.car AND ano != 0  ORDER BY ano LIMIT 1) foo
 WHERE (a.gid % :var_num_proc) = :var_proc
