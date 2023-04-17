cab3cef96e2968741967c1b14b9cf7749b435187
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-04-17 10:46:10 -0300

    Update proc3_antigo_1.sql

--- irregularidades/proc3_antigo_1.sql
+++ irregularidades/proc3_antigo_1.sql
@@ -1,12 +1,13 @@
 \echo proc_1
 
-DROP TABLE IF EXISTS irregularidades.proc3_step14_ano_ocupacao;
-CREATE TABLE irregularidades.proc3_step14_ano_ocupacao (
+
+DROP TABLE IF EXISTS irregularidades.proc3_step14_ano_ocupacaov2;
+CREATE TABLE irregularidades.proc3_step14_ano_ocupacaov2 (
 gid bigint null, 
 desmatamento int null, 
 is_recente boolean null
 );
 
 
-CREATE INDEX proc3_step14_ano_ocupacao_gid_idx ON irregularidades.proc3_step14_ano_ocupacao USING btree (gid);
+CREATE INDEX proc3_step14_ano_ocupacaov2_gid_idx ON irregularidades.proc3_step14_ano_ocupacaov2 USING btree (gid);
 
