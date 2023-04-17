735c6c5a0481a6011cec324c69b5062939faf7b8
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-04-17 16:02:41 -0300

    Update proc8_desm_recente_1.sql

--- irregularidades/proc8_desm_recente_1.sql
+++ irregularidades/proc8_desm_recente_1.sql
@@ -1,12 +1,12 @@
 \echo proc_1
 
-DROP TABLE IF EXISTS irregularidades.proc3_step14_desmatamento_recente;
-CREATE TABLE irregularidades.proc3_step14_desmatamento_recente (
+DROP TABLE IF EXISTS irregularidades.proc8_step14_desmatamento_recente;
+CREATE TABLE irregularidades.proc8_step14_desmatamento_recente (
 gid bigint null, 
 desmatamento int null, 
 is_desm_recente boolean null
 );
 
 
-CREATE INDEX proc3_step14_desmatamento_recente_gid_idx ON irregularidades.proc3_step14_desmatamento_recente USING btree (gid);
+CREATE INDEX proc8_step14_desmatamento_recente_gid_idx ON irregularidades.proc8_step14_desmatamento_recente USING btree (gid);
 
