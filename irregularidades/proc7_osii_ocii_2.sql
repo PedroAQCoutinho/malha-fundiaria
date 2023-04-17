62bcc3ec61f6a2d85db99ecd503f4fa0f948adf5
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-04-17 10:46:32 -0300

    Update proc7_osii_ocii_2.sql

--- irregularidades/proc7_osii_ocii_2.sql
+++ irregularidades/proc7_osii_ocii_2.sql
@@ -1,21 +1,18 @@
 
 \echo RUN :var_proc
-INSERT INTO irregularidades.proc7_step14_categorizacao_1404 (car, tipo_imove,  area_imovel, am_legal, 
-cd_mun, cd_bioma, nm_agrup, nm_cat_fund, m_fiscal, is_grande, is_recente, is_desm_recente, desmatamento, is_local_restrito, tipo_irregularidade, geom )
+INSERT INTO irregularidades.proc7_step14_categorizacao_1404 (car,  area_imovel, am_legal, cd_mun, cd_bioma, nm_agrup, nm_cat_fund, is_grande, is_recente, is_desm_recente, desmatamento, is_local_restrito, tipo_irregularidade, geom )
 SELECT 
 a.gid car, 
-tipo_imove ,
 a.area area_imovel, 
 am_legal, 
 cd_mun, 
 cd_bioma, 
 d.nm_agrup, 
 d.nm_cat_fund , 
-a.m_fiscal,
 is_grande, 
 is_recente , 
 is_desm_recente,
-b.desmatamento, 
+e.desmatamento,  
 is_local_restrito, 
 CASE 
 	WHEN tipo_imove = 'IRU' AND ( d.nm_agrup IN ('publica_afetada', 'glebas_publicas', 'vazio', 'publica afetada_coletiva_privada'))
@@ -39,9 +36,9 @@ LEFT JOIN irregularidades.proc3_step14_ano_ocupacao b ON a.gid = b.gid
 LEFT JOIN irregularidades.proc4_step14_tamanho_ocupacao c ON a.gid = c.car
 LEFT JOIN irregularidades.proc5_step14_categoria_fundiaria d ON a.gid = d.car
 LEFT JOIN irregularidades.proc6_step14_local_restrito e ON a.gid = e.car
-LEFT JOIN irregularidades.proc8_step14_desmatamento_recente f ON a.gid = f.gid
+LEFT JOIN irregularidades.proc8_step14_desmatamento_recente f ON a.gid = e.car
 LEFT JOIN LATERAL (SELECT * FROM irregularidades.proc23_step14_desmatamento_anual h 
-WHERE h.car = a.gid ORDER BY h.area_count DESC LIMIT 1 ) foo 
+WHERE h.car = a.gid ORDER BY h.area_count DESC LIMIT 1 ) foo
 ON TRUE WHERE (a.gid % :var_num_proc) = :var_proc
 
 
