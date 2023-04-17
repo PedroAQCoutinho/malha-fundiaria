4f6b103f1bb0901edd5e5d9aa294d7021322dd79
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-04-17 12:04:42 -0300

    Update bash_execproc.sh

--- irregularidades/bash_execproc.sh
+++ irregularidades/bash_execproc.sh
@@ -2,26 +2,7 @@
 
 userName=postgres
 databaseName=malha_fundiaria
-numProc=32
-export PGPASSWORD='gpp-es@lq'
-procName=proc3_antigo
-
-
-psql -U $userName -d $databaseName -f ${procName}_1.sql
-
-#If sql 2 exists execute it
-for ((i=0; i < ${numProc}; i++))
-do
-    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_2.sql &
-done
-wait
-
-
-
-
-userName=postgres
-databaseName=malha_fundiaria
-numProc=64
+numProc=240
 export PGPASSWORD='gpp-es@lq'
 procName=proc7_osii_ocii
 
@@ -34,3 +15,5 @@ do
     psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_2.sql &
 done
 wait
+
+
