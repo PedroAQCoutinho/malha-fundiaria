a69e28d1ac22689c20c89db85be4e5b6f30df661
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-04-28 17:01:29 -0300

    Update functionsv2.sql

--- procs_split_fundiario/functionsv2.sql
+++ procs_split_fundiario/functionsv2.sql
@@ -32,7 +32,7 @@ CREATE OR REPLACE FUNCTION sample_data(grid int)
   LANGUAGE plpgsql AS
 $func$
 BEGIN
-   EXECUTE format('CREATE TABLE public.grid_%s_data AS 
+   EXECUTE format('CREATE TEMPORARY TABLE griddata_%s AS 
 				SELECT * FROM (SELECT * FROM get_table_data(%s) WHERE geom IS NOT NULL
 				UNION ALL 
 				SELECT cd_grid, ''GRID'' original_layer, ao.gid original_gid, geom 
@@ -48,13 +48,13 @@ CREATE OR REPLACE FUNCTION f_dump(grid int)
   LANGUAGE plpgsql AS
 $func$
 BEGIN
-   EXECUTE format('CREATE TABLE public.dump_%s AS
-      SELECT a.gid, cd_grid, cd_mun, am_legal, 1 cd_bioma, original_layer, original_gid,
+   EXECUTE format('CREATE TEMPORARY TABLE fdump_%s AS
+      SELECT a.gid, cd_grid, cd_mun, am_legal, original_layer, original_gid,
       st_collectionextract((ST_Dump(ST_Intersection(ST_SETSRID(a.geom, 4326), ST_SETSRID(b.geom, 4326)))).geom, 3) geom 
-      FROM public.grid_%s_data a LEFT JOIN grid.adm2_overlay b
+      FROM griddata_%s a LEFT JOIN grid.adm2_overlay b
       ON ST_Intersects(ST_SETSRID(a.geom, 4326), ST_SETSRID(b.geom, 4326)) WHERE  b.cd_grid = cast(%s as integer);
-      CREATE INDEX dump_%s_gid_idx ON dump_%s USING btree (gid);
-      CREATE INDEX dump_%s_geom_idx ON dump_%s USING GIST (geom);', 
+      CREATE INDEX fdump_%s_gid_idx ON fdump_%s USING btree (gid);
+      CREATE INDEX fdump_%s_geom_idx ON fdump_%s USING GIST (geom);', 
      grid, grid, grid, grid, grid, grid, grid);
 END
 $func$;
@@ -68,12 +68,12 @@ CREATE OR REPLACE FUNCTION split_polygons(grid int)
 $func$
 BEGIN
    EXECUTE format('
-      CREATE TABLE proc1_split_polygons_%s AS 
+      CREATE TEMPORARY TABLE split_polygons_%s AS 
       SELECT st_asbinary((ST_Dump(ST_Split(ST_Union(ST_Force2d(geom)), ST_Union(st_exteriorring(ST_Force2d(geom)))))).geom) bin, 
       (ST_Dump(ST_Split(ST_Union(ST_Force2d(geom)), ST_Union(st_exteriorring(ST_Force2d(geom)))))).geom 
-      FROM dump_%s;
-      CREATE INDEX proc1_split_polygons_%s_bin_idx ON proc1_split_polygons_%s USING hash (bin);
-      CREATE INDEX proc1_split_polygons_%s_geom_idx ON proc1_split_polygons_%s USING GIST (geom);', 
+      FROM fdump_%s;
+      CREATE INDEX split_polygons_%s_bin_idx ON split_polygons_%s USING hash (bin);
+      CREATE INDEX split_polygons_%s_geom_idx ON split_polygons_%s USING GIST (geom);', 
      grid,grid,grid,grid,grid,grid);
 END
 $func$; 
@@ -86,32 +86,18 @@ CREATE OR REPLACE FUNCTION inserto_array_agg(grid int)
 $func$
 BEGIN
    EXECUTE format('
-        INSERT INTO car.proc2_array_agg (original_gid, original_layer, cd_grid, cd_mun, am_legal, cd_bioma, area, geom) 
+        INSERT INTO malhav2.proc2_malhav2 (original_gid, original_layer, cd_grid, area, geom) 
         SELECT
         array_agg(b.original_gid) original_gid,
         array_agg(original_layer) original_layer,
         cd_grid,
-        cd_mun, 
-        am_legal, 
-        cd_bioma, 
         ST_Area(ST_Union(ST_Force2d(a.geom))::geography)/10000 area,
         ST_Union(ST_Force2d(a.geom)) geom
-        FROM proc1_split_polygons_%s a 
-        LEFT JOIN dump_%s b ON ST_Intersects(ST_Buffer(a.geom::geography, -5), b.geom) 
+        FROM split_polygons_%s a 
+        LEFT JOIN fdump_%s b ON ST_Intersects(ST_Buffer(ST_Force2d(a.geom)::geography, -5), ST_Force2d(b.geom)) 
         GROUP BY bin, cd_grid;', grid, grid);
 END
 $func$;
-
-
-
-
-
-
-SELECT sample_data(264063)
-SELECT f_dump(264063)
-SELECT split_polygons(264063)
-select inserto_array_agg(264063)
-
 
 
 
