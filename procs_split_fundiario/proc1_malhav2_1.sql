90935e39d6fbca7c306d33c2df2fdbd5d636d57e
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-04-28 16:53:19 -0300

    Update proc1_malhav2_1.sql

--- procs_split_fundiario/proc1_malhav2_1.sql
+++ procs_split_fundiario/proc1_malhav2_1.sql
@@ -1,22 +1,23 @@
 
-DROP TABLE IF EXISTS car.proc2_array_agg;
-CREATE TABLE car.proc2_array_agg (
+DROP TABLE IF EXISTS malhav2.proc2_malhav2;
+CREATE TABLE malhav2.proc2_malhav2 (
 
 gid serial NOT null,
 original_gid int[] null,
 original_layer text[] null,
 cd_grid integer null, 
-cd_mun integer null, 
-am_legal boolean null ,
-cd_bioma int null, 
 area decimal null,
 geom geometry null
 
 
 );
 
-CREATE INDEX proc2_array_agg_bin_idx ON car.proc2_array_agg USING btree (gid);
-CREATE INDEX proc2_array_agg_geom_idx ON car.proc2_array_agg USING GIST (geom);
+CREATE INDEX proc2_malhav2_gid_idx ON malhav2.proc2_malhav2 USING btree (gid);
+CREATE INDEX proc2_malhav2_geom_idx ON malhav2.proc2_malhav2 USING GIST (geom);
+
+CREATE INDEX proc2_malhav2_cd_grid_idx ON malhav2.proc2_malhav2 USING btree (cd_grid);
+
+
 
 
 
