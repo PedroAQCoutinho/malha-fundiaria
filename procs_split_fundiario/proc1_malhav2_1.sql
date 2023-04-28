358dc641f715bc01f280e41d42a9a73a164600d1
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-04-28 16:51:17 -0300

    Create proc1_malhav2_1.sql

---
+++ procs_split_fundiario/proc1_malhav2_1.sql
@@ -0,0 +1,32 @@
+
+DROP TABLE IF EXISTS car.proc2_array_agg;
+CREATE TABLE car.proc2_array_agg (
+
+gid serial NOT null,
+original_gid int[] null,
+original_layer text[] null,
+cd_grid integer null, 
+cd_mun integer null, 
+am_legal boolean null ,
+cd_bioma int null, 
+area decimal null,
+geom geometry null
+
+
+);
+
+CREATE INDEX proc2_array_agg_bin_idx ON car.proc2_array_agg USING btree (gid);
+CREATE INDEX proc2_array_agg_geom_idx ON car.proc2_array_agg USING GIST (geom);
+
+
+
+
+
+
+
+
+
+
+
+
+
