6e01cab309844eb0cfc791f62f951dfeb9e0d86c
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-04-28 12:06:38 -0300

    Update proc2_array_agg_1.sql

--- procs_car/proc2_array_agg_1.sql
+++ procs_car/proc2_array_agg_1.sql
@@ -18,62 +18,6 @@ CREATE INDEX proc2_array_agg_bin_idx ON car.proc2_array_agg USING btree (gid);
 CREATE INDEX proc2_array_agg_geom_idx ON car.proc2_array_agg USING GIST (geom);
 
 
-CREATE OR REPLACE FUNCTION car_dump(t_name varchar(30))
-  RETURNS VOID
-  LANGUAGE plpgsql AS
-$func$
-BEGIN
-   EXECUTE format('CREATE TEMPORARY TABLE car_dump_%s AS
-      SELECT a.gid, cd_grid, cd_mun, am_legal, cd_bioma,
-      st_collectionextract((ST_Dump(ST_Intersection(a.valid_geom, b.geom))).geom, 3) geom 
-      FROM dados_brutos.valid_sicar_imovel a LEFT JOIN grid.adm3_overlay b
-      ON ST_Intersects(a.valid_geom, b.geom) WHERE  b.cd_grid = cast(%s as integer);
-      CREATE INDEX car_dump_%s_gid_idx ON car_dump_%s USING btree (gid);
-      CREATE INDEX car_dump_%s_geom_idx ON car_dump_%s USING GIST (geom);', t_name, t_name, t_name, t_name, t_name, t_name);
-END
-$func$;
-
-
-
-
-
-CREATE OR REPLACE FUNCTION split_polygons(t_name varchar(30))
-  RETURNS VOID
-  LANGUAGE plpgsql AS
-$func$
-BEGIN
-   EXECUTE format('
-      CREATE TEMPORARY TABLE proc1_split_polygons_%s AS 
-      SELECT st_asbinary((ST_Dump(ST_Split(ST_Union(geom), ST_Union(st_exteriorring(geom))))).geom) bin, 
-      (ST_Dump(ST_Split(ST_Union(geom), ST_Union(st_exteriorring(geom))))).geom 
-      FROM car_dump_%s;
-      CREATE INDEX proc1_split_polygons_%s_bin_idx ON proc1_split_polygons_%s USING hash (bin);
-      CREATE INDEX proc1_split_polygons_%s_geom_idx ON proc1_split_polygons_%s USING GIST (geom);', t_name,t_name,t_name,t_name,t_name,t_name);
-END
-$func$;
-
-
-
-
-
-CREATE OR REPLACE FUNCTION inserto_array_agg(t_name varchar(30))
-  RETURNS VOID
-  LANGUAGE plpgsql AS
-$func$
-BEGIN
-   EXECUTE format('
-      INSERT INTO car.proc2_array_agg (geom, original_gid, cd_grid, cd_mun, am_legal, cd_bioma, area) 
-      SELECT ST_Union(ST_Force2d(a.geom)) geom,
-      array_agg(b.gid) original_gid, cd_grid, cd_mun, am_legal, cd_bioma,
-      ST_Area(ST_Union(ST_Force2d(a.geom))::geography) area  FROM proc1_split_polygons_%s a 
-      LEFT JOIN car_dump_%s b ON ST_Intersects(ST_Buffer(a.geom::geography, -5), b.geom) 
-      GROUP BY bin, cd_grid, cd_mun, am_legal,cd_bioma;', t_name, t_name);
-END
-$func$;
-
-
-
-
 
 
 
