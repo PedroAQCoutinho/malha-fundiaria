675e058f857b224e09e2631e5f7b20613db2220a
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-04 17:49:44 -0300

    Update proc6_malha_2.sql

--- proc_categorias_fundiarias/proc6_malha_2.sql
+++ proc_categorias_fundiarias/proc6_malha_2.sql
@@ -2,10 +2,10 @@
 
 
 INSERT INTO malhav2.proc6_malha (gid,original_gid ,original_layer,cd_grid,area, is_car,is_faixafronteira,is_militar,
-is_massasdagua,is_quilombola,is_ucpi,is_ucus,is_ucusapa,is_ti,is_imovel,is_gleba,is_assentamento,
+is_massadagua,is_quilombola,is_ucpi,is_ucus,is_ucusapa,is_ti,is_imovel,is_gleba,is_assentamento,
 original_layer_label,nm_cat_fund,nm_agrup,geom)
 SELECT gid, original_gid, a.original_layer, cd_grid, area, is_car, is_faixafronteira,
-is_militar, is_massasdagua, is_quilombola, is_ucpi, is_ucus, is_ucusapa, is_ti, is_imovel,
+is_militar, is_massadagua, is_quilombola, is_ucpi, is_ucus, is_ucusapa, is_ti, is_imovel,
 is_gleba, is_assentamento, original_layer_label, nm_cat_fund, nm_agrup, geom
 FROM malhav2.proc5_malha a 
 LEFT JOIN auxiliares.nomenclatura_categorias_e_agrupamentos_fundiarios b 
