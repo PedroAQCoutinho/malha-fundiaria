83003667252a848eda27d41957b77dd252c0658a
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-04 17:41:50 -0300

    Update proc6_malha_2.sql

--- proc_categorias_fundiarias/proc6_malha_2.sql
+++ proc_categorias_fundiarias/proc6_malha_2.sql
@@ -1,10 +1,9 @@
-INSERT INTO malhav2.proc6_malha
-
 \echo RUN proc6_malha :var_proc
 
 
-INSERT INTO malhav2.proc6_malha (gid,original_gid ,original_layer,cd_grid,area, is_car,is_faixafronteira,is_militar,is_massasdagua,is_quilombola,is_ucpi,
-is_ucus,is_ucusapa,is_ti,is_imovel,is_gleba,is_assentamento,original_layer_label,nm_cat_fund,nm_agrup,geom)
+INSERT INTO malhav2.proc6_malha (gid,original_gid ,original_layer,cd_grid,area, is_car,is_faixafronteira,is_militar,
+is_massasdagua,is_quilombola,is_ucpi,is_ucus,is_ucusapa,is_ti,is_imovel,is_gleba,is_assentamento,
+original_layer_label,nm_cat_fund,nm_agrup,geom)
 SELECT gid, original_gid, original_layer, cd_grid, area, is_car, is_faixafronteira,
 is_militar, is_massasdagua, is_quilombola, is_ucpi, is_ucus, is_ucusapa, is_ti, is_imovel,
 is_gleba, is_assentamento, original_layer_label, nm_cat_fund, nm_agrup, geom
