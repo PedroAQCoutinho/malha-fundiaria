76af5024800eb495426be19cb1978c7292dce16e
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-04 17:02:37 -0300

    Create proc5_malha_2.sql

---
+++ proc_categorias_fundiarias/proc5_malha_2.sql
@@ -0,0 +1,81 @@
+INSERT INTO malhav2.proc5_malha
+SELECT gid, original_gid , original_layer , cd_grid, area,  
+CASE 
+	WHEN 'CAR' = ANY(original_layer) THEN TRUE
+	ELSE FALSE
+END is_car,
+CASE 
+	WHEN 'FF' = ANY(original_layer) THEN TRUE
+	ELSE FALSE
+END is_faixafronteira,
+CASE 
+	WHEN 'MI' = ANY(original_layer) THEN TRUE
+	ELSE FALSE
+END is_militar,
+CASE 
+	WHEN 'MD' = ANY(original_layer) THEN TRUE
+	ELSE FALSE
+END is_massadagua,
+CASE 
+	WHEN 'QUI' = ANY(original_layer) THEN TRUE
+	ELSE FALSE
+END is_quilombola,
+CASE 
+	WHEN 'UCPI' = ANY(original_layer) THEN TRUE
+	ELSE FALSE
+END is_ucpi,
+CASE 
+	WHEN 'UCUS' = ANY(original_layer) THEN TRUE
+	ELSE FALSE
+END is_ucus,
+CASE 
+	WHEN 'UCUSAPA' = ANY(original_layer) THEN TRUE
+	ELSE FALSE
+END is_ucusapa,
+CASE 
+	WHEN 'TI' = ANY(original_layer) THEN TRUE
+	ELSE FALSE
+END is_ti,
+CASE 
+	WHEN 'TERRALEG' = ANY(original_layer) THEN TRUE
+	WHEN 'SIGEFPRIV' = ANY(original_layer) THEN TRUE
+	WHEN 'SNCIPRIV' = ANY(original_layer) THEN TRUE
+	ELSE FALSE
+END is_imovel,
+CASE 
+	WHEN 'GLEBAEST' = ANY(original_layer) THEN TRUE
+	WHEN 'GLEBAFED' = ANY(original_layer) THEN TRUE
+	WHEN 'SIGEFPUB' = ANY(original_layer) THEN TRUE
+	WHEN 'SNCIPUB' = ANY(original_layer) THEN TRUE
+	ELSE FALSE
+END is_gleba,
+CASE 
+	WHEN 'ASSENREC' = ANY(original_layer) THEN TRUE
+	WHEN 'ASSENFED' = ANY(original_layer) THEN TRUE
+	ELSE FALSE
+END is_assentamento,
+anyarray_sort(anyarray_uniq(
+array_replace(
+array_replace(
+array_replace(
+array_replace(
+array_replace(
+array_replace(
+array_replace(
+array_replace(
+array_replace(array_remove(array_remove(array_remove(original_layer, 'FF'),'CAR'), 'MD'), 'ASSENREC', 'ASSEN'), 
+'ASSENFED', 'ASSEN'),
+'GLEBAEST', 'GLEBA'),
+'GLEBAFED', 'GLEBA'), 
+'SIGEFPUB', 'GLEBA'), 
+'SNCIPUB', 'GLEBA'), 
+'TERRALEG', 'IMOVEL'), 
+'SIGEFPRIV', 'IMOVEL'), 
+'SNCIPRIV', 'IMOVEL'))) original_layer_label,
+geom
+FROM malhav2.proc2_malhav2 pm 
+WHERE (a.gid % :var_num_proc) = :var_proc
+
+
+
+
