23d277d8321124ff3cea5017c408c5d534995874
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-05 13:02:53 -0300

    Update cte_verify_running.sh

--- procs_split_fundiario/cte_verify_running.sh
+++ procs_split_fundiario/cte_verify_running.sh
@@ -8,9 +8,12 @@ pid=$(cat "$pid_file")
 # Verifique se o PID está em execução
 if kill -0 "$pid" > /dev/null 2>&1
 then
-    echo "ok"
+    echo "Already running !"
 else
     # Execute o script bash_execproc.sh se o PID não estiver em execução
-    echo "not running, then run !" 
+    echo "Not running, then run !" 
     bash /home/pedro_alves_coutinho_usp_br/malha-fundiaria/procs_split_fundiario/bash_queue_run.sh > log 2>&1 &
 fi
+
+
+
