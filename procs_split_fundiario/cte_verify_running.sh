ba9cb4374c2f13e81f99354b2b95bb12b0749f9e
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-05 16:35:04 -0300

    Update cte_verify_running.sh

--- procs_split_fundiario/cte_verify_running.sh
+++ procs_split_fundiario/cte_verify_running.sh
@@ -18,7 +18,7 @@ else
     echo "Not running, then run !" 
     echo "Time: $(date '+%Y-%m-%d %T') - O arquivo cd_grid.txt ainda contém $(wc -l < cd_grid.txt) linhas porém agora iniciando uma nova contagem..."
     echo "-------------------------"
-    bash /home/pedro_alves_coutinho_usp_br/malha-fundiaria/procs_split_fundiario/bash_queue_run.sh > log 2>&1 &
+    bash /home/pedro_alves_coutinho_usp_br/malha-fundiaria/procs_split_fundiario/bash_queue_run.sh > /home/pedro_alves_coutinho_usp_br/malha-fundiaria/procs_split_fundiario/log 2>&1 &
 fi
 
 
