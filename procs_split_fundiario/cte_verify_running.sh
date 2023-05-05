ca4e296742f27ae3ee6c8b208c34c0eade201d50
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-05 13:07:52 -0300

    Update cte_verify_running.sh

--- procs_split_fundiario/cte_verify_running.sh
+++ procs_split_fundiario/cte_verify_running.sh
@@ -9,9 +9,13 @@ pid=$(cat "$pid_file")
 if kill -0 "$pid" > /dev/null 2>&1
 then
     echo "Already running !"
+    echo "Time: $(date '+%Y-%m-%d %T') - O arquivo cd_grid.txt ainda contém $(wc -l < cd_grid.txt) linhas."
+
+
 else
     # Execute o script bash_execproc.sh se o PID não estiver em execução
     echo "Not running, then run !" 
+    echo "Time: $(date '+%Y-%m-%d %T') - O arquivo cd_grid.txt ainda contém $(wc -l < cd_grid.txt) linhas porém agora iniciando uma nova contagem..."
     bash /home/pedro_alves_coutinho_usp_br/malha-fundiaria/procs_split_fundiario/bash_queue_run.sh > log 2>&1 &
 fi
 
