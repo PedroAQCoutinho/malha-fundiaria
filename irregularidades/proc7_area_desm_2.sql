cfdebc4f004b1aa6d857ce50d01ee325170e961e
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-08 09:25:59 -0300

    Create proc7_area_desm_2.sql

---
+++ irregularidades/proc7_area_desm_2.sql
@@ -0,0 +1,8 @@
+\echo RUN :var_proc
+
+INSERT INTO irregularidades.proc8_step14_desmatamento_recente (gid, desmatamento, is_desm_recente)
+SELECT gid, ano, CASE WHEN ano <= 8 THEN FALSE WHEN ano > 8 THEN FALSE END is_desm_recente
+FROM dados_brutos.valid_sicar_imovel a, LATERAL 
+(SELECT * FROM irregularidades.proc23_step14_desmatamento_anual b 
+WHERE a.gid = b.car AND ano != 0  ORDER BY gid_car, ano DESC LIMIT 1) foo
+WHERE (a.gid % :var_num_proc) = :var_proc
