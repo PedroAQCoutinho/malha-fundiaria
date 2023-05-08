40d66428961ec829c4c0c9b86845572888b6122b
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-08 16:52:42 -0300

    Update proc1_area_raw.sql

--- irregularidades/proc1_area_raw.sql
+++ irregularidades/proc1_area_raw.sql
@@ -3,15 +3,15 @@ CREATE TABLE irregularidades.proc1_malhav2
 (
 cat_fund int NULL,
 car bigint NULL,
-desmatamento int NULL,
+uso int NULL,
 count int NULL
 );
 
 CREATE INDEX proc1_malhav2_car_idx ON irregularidades.proc1_malhav2 USING btree (car);
 CREATE INDEX proc1_malhav2_cat_fund_idx ON irregularidades.proc1_malhav2 USING btree (cat_fund);
-CREATE INDEX proc1_malhav2_desmatamento_idx ON irregularidades.proc1_malhav2 USING btree (desmatamento);
+CREATE INDEX proc1_malhav2_uso_idx ON irregularidades.proc1_malhav2 USING btree (uso);
 
 
-INSERT INTO irregularidades.proc1_malhav2 (cat_fund, car, desmatamento, count)
-SELECT cat_fund, car, desmatamento, count FROM public.proc1_row_by_row
+INSERT INTO irregularidades.proc1_malhav2 (cat_fund, car, uso, count)
+SELECT cat_fund, car, uso, count FROM public.proc1_row_by_row_mapbiomas
 WHERE cat_fund IS NOT NULL
