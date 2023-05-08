9c93948566ed4dd1b0e5b98c845ef342a35f89d3
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-08 16:52:48 -0300

    Update proc2_3_desmatamento_anual_1.sql

--- irregularidades/proc2_3_desmatamento_anual_1.sql
+++ irregularidades/proc2_3_desmatamento_anual_1.sql
@@ -1,12 +1,12 @@
 
-DROP TABLE IF EXISTS irregularidades.proc23_step14_desmatamento_anual ;
-CREATE TABLE irregularidades.proc23_step14_desmatamento_anual
+DROP TABLE IF EXISTS irregularidades.proc23_malha_uso_anual ;
+CREATE TABLE irregularidades.proc23_malha_uso_anual
 (
 cat_fund integer null, 
 iscar int NULL,
 gid_car integer null , 
 car integer NULL,   
-ano integer null, 
+uso integer null, 
 area_count decimal null, 
 cd_grid INT NULL , 
 cd_mun INT null, 
@@ -18,8 +18,10 @@ orilabel TEXT[] NULL
 );
 
 
-CREATE INDEX proc23_step14_desmatamento_anual_car_idx ON irregularidades.proc23_step14_desmatamento_anual USING btree (car);
-CREATE INDEX proc23_step14_desmatamento_anual_cat_fund_idx ON irregularidades.proc23_step14_desmatamento_anual USING btree (cat_fund);
-CREATE INDEX proc23_step14_desmatamento_anual_orilabel_idx ON irregularidades.proc23_step14_desmatamento_anual USING btree (orilabel);
-CREATE INDEX proc23_step14_desmatamento_anual_nm_cat_fund_idx ON irregularidades.proc23_step14_desmatamento_anual USING btree (nm_cat_fund);
-CREATE INDEX proc23_step14_desmatamento_anual_nm_agrup_idx ON irregularidades.proc23_step14_desmatamento_anual USING btree (nm_agrup);
+CREATE INDEX proc23_malha_uso_anual_car_idx ON irregularidades.proc23_malha_uso_anual USING btree (car);
+CREATE INDEX proc23_malha_uso_anual_cat_fund_idx ON irregularidades.proc23_malha_uso_anual USING btree (cat_fund);
+CREATE INDEX proc23_malha_uso_anual_orilabel_idx ON irregularidades.proc23_malha_uso_anual USING btree (orilabel);
+CREATE INDEX proc23_malha_uso_anual_nm_cat_fund_idx ON irregularidades.proc23_malha_uso_anual USING btree (nm_cat_fund);
+CREATE INDEX proc23_malha_uso_anual_nm_agrup_idx ON irregularidades.proc23_malha_uso_anual USING btree (nm_agrup);
+CREATE INDEX proc23_malha_uso_anual_uso_idx ON irregularidades.proc23_malha_uso_anual USING btree (uso);
+
