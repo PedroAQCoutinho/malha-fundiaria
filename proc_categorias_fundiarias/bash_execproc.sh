baea94f2b02a101436ab3486fbd543d44db7b038
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-08 10:31:24 -0300

    Update bash_execproc.sh

--- proc_categorias_fundiarias/bash_execproc.sh
+++ proc_categorias_fundiarias/bash_execproc.sh
@@ -5,7 +5,7 @@ export PGPASSWORD='gpp-es@lq'
 procName=proc6_malha
 
 
-psql -U $userName -d $databaseName -f ${procName}_1.sql
+#psql -U $userName -d $databaseName -f ${procName}_1.sql
 
 
 #If sql 2 exists execute it
