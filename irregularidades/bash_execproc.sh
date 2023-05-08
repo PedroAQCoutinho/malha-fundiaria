1f69799f7cc81381f241135febbd02eb6f358b77
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-08 16:55:28 -0300

    Update bash_execproc.sh

--- irregularidades/bash_execproc.sh
+++ irregularidades/bash_execproc.sh
@@ -2,10 +2,48 @@
 
 userName=postgres
 databaseName=malha_fundiaria
-numProc=64
-export PGPASSWORD='gpp-es@lq'
-procName=proc7_osii_ocii
+numProc=16
 
+
+
+
+procName=proc1_area_raw
+export PGPASSWORD='gpp-es@lq'
+
+
+psql -U $userName -d $databaseName -f ${procName}.sql
+
+
+procName=proc2_1_desmatamento_anual
+export PGPASSWORD='gpp-es@lq'
+
+
+
+psql -U $userName -d $databaseName -f ${procName}.sql
+
+
+procName=proc2_2_desmatamento_anual
+export PGPASSWORD='gpp-es@lq'
+
+psql -U $userName -d $databaseName -f ${procName}.sql
+
+
+procName=proc2_3_desmatamento_anual
+export PGPASSWORD='gpp-es@lq'
+
+psql -U $userName -d $databaseName -f ${procName}_1.sql
+
+
+#If sql 2 exists execute it
+for ((i=0; i < ${numProc}; i++))
+do
+    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_2.sql &
+done
+wait
+
+
+procName=proc3_antigo
+export PGPASSWORD='gpp-es@lq'
 
 psql -U $userName -d $databaseName -f ${procName}_1.sql
 
@@ -15,5 +53,7 @@ do
     psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_2.sql &
 done
 wait
+
+
 
 
