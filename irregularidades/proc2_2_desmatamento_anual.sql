d90b394aae692b4aa8f3acf9276994edded2b496
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-09 12:17:33 -0300

    Update proc2_2_desmatamento_anual.sql

--- irregularidades/proc2_2_desmatamento_anual.sql
+++ irregularidades/proc2_2_desmatamento_anual.sql
@@ -6,7 +6,7 @@ SELECT cat_fund, CASE WHEN car IS NOT NULL THEN 1 ELSE 0 END iscar, car gid_car,
 uso,a.area area_count,  cd_grid, c.cd_mun, c.am_legal, c.cd_bioma , nm_agrup, nm_cat_fund, orilabel
 FROM irregularidades.proc21_malha_uso_anual a
 LEFT JOIN car.proc2_array_agg b ON car = gid
-LEFT JOIN malhav2.proc6_malha c ON c.gid = cat_fund 
+LEFT JOIN malhav2.proc6_malha c ON c.gid = cat_fund;
 
 CREATE INDEX proc22_malha_uso_anual_car_idx ON irregularidades.proc22_malha_uso_anual USING btree (car);
 CREATE INDEX proc22_malha_uso_anual_cat_fund_idx ON irregularidades.proc22_malha_uso_anual USING btree (cat_fund);
