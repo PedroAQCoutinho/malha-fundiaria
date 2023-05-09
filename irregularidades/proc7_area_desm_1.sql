849c6ac7cb60ed0f0cba6642a97ea6c836d4e19e
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-09 12:18:47 -0300

    Update proc7_area_desm_1.sql

--- irregularidades/proc7_area_desm_1.sql
+++ irregularidades/proc7_area_desm_1.sql
@@ -1,12 +1,12 @@
-\echo proc_1
+\echo area antrop
 
-DROP TABLE IF EXISTS irregularidades.proc8_step14_desmatamento_recente;
-CREATE TABLE irregularidades.proc8_step14_desmatamento_recente (
+DROP TABLE IF EXISTS irregularidades.proc8_area_antrop;
+CREATE TABLE irregularidades.proc8_area_antrop (
 gid bigint null, 
 desmatamento int null, 
 is_desm_recente boolean null
 );
 
 
-CREATE INDEX proc8_step14_desmatamento_recente_gid_idx ON irregularidades.proc8_step14_desmatamento_recente USING btree (gid);
+CREATE INDEX proc8_area_antrop_gid_idx ON irregularidades.proc8_area_antrop USING btree (gid);
 
