4ef1717053609a1b4e4d6de5699d0216fdf4c73a
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-09 15:34:42 -0300

    Update proc2_3_desmatamento_anual_1.sql

--- irregularidades/proc2_3_desmatamento_anual_1.sql
+++ irregularidades/proc2_3_desmatamento_anual_1.sql
@@ -19,7 +19,6 @@ nm_cat_fund TEXT NULL
 
 CREATE INDEX proc23_malha_uso_anual_car_idx ON irregularidades.proc23_malha_uso_anual USING btree (car);
 CREATE INDEX proc23_malha_uso_anual_cat_fund_idx ON irregularidades.proc23_malha_uso_anual USING btree (cat_fund);
-CREATE INDEX proc23_malha_uso_anual_orilabel_idx ON irregularidades.proc23_malha_uso_anual USING btree (orilabel);
 CREATE INDEX proc23_malha_uso_anual_nm_cat_fund_idx ON irregularidades.proc23_malha_uso_anual USING btree (nm_cat_fund);
 CREATE INDEX proc23_malha_uso_anual_nm_agrup_idx ON irregularidades.proc23_malha_uso_anual USING btree (nm_agrup);
 CREATE INDEX proc23_malha_uso_anual_uso_idx ON irregularidades.proc23_malha_uso_anual USING btree (uso);
