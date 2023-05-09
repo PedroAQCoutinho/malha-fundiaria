a07fd60589ca53c04b2de4e1f4864a625e5bf4e4
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-09 11:02:10 -0300

    Update functionsv2.sql

--- procs_split_fundiario/functionsv2.sql
+++ procs_split_fundiario/functionsv2.sql
@@ -8,7 +8,7 @@ DECLARE
 BEGIN
 	FOR tab IN (SELECT nm_dado_original_valid_geom FROM auxiliares.inputs a WHERE a.nm_dado_original_valid_geom IS NOT NULL) LOOP	
     RETURN QUERY EXECUTE format('SELECT cd_grid, ''%s'' original_layer, 
-	b.gid original_gid, ST_CollectionExtract(ST_Intersection(ST_SETSRID(b.valid_geom, 4326), ST_SETSRID(a.geom, 4326)), 3) geom FROM grid.adm3_overlay a
+	b.gid original_gid, ST_CollectionExtract(ST_Intersection(ST_SETSRID(b.valid_geom, 4326), ST_SETSRID(a.geom, 4326)), 3) geom FROM grid.adm2_overlay a
 	LEFT JOIN dados_brutos.%I b ON ST_Intersects(ST_SETSRID(a.geom, 4326), ST_SETSRID(b.valid_geom, 4326)) WHERE cd_grid = %s AND am_legal %s', 
 	(SELECT a.label FROM auxiliares.inputs a WHERE a.nm_dado_original_valid_geom = tab), 
 	tab, 
@@ -34,7 +34,7 @@ BEGIN
 				SELECT * FROM (SELECT * FROM get_table_data(%s) WHERE geom IS NOT NULL
 				UNION ALL 
 				SELECT cd_grid, ''GRID'' original_layer, ao.gid original_gid, geom 
-				FROM grid.adm3_overlay ao WHERE cd_grid = %s and am_legal) foo;', 
+				FROM grid.adm2_overlay ao WHERE cd_grid = %s and am_legal) foo;', 
      grid, grid, grid);
 END
 $func$;
@@ -47,9 +47,9 @@ CREATE OR REPLACE FUNCTION f_dump(grid int)
 $func$
 BEGIN
    EXECUTE format('CREATE TEMPORARY TABLE fdump_%s AS
-      SELECT a.gid, cd_grid, cd_mun, cd_bioma, am_legal, original_layer, original_gid,
+      SELECT a.gid, cd_grid, am_legal, original_layer, original_gid,
       st_collectionextract((ST_Dump(ST_Intersection(ST_SETSRID(a.geom, 4326), ST_SETSRID(b.geom, 4326)))).geom, 3) geom 
-      FROM griddata_%s a LEFT JOIN grid.adm3_overlay b
+      FROM griddata_%s a LEFT JOIN grid.adm2_overlay b
       ON ST_Intersects(ST_SETSRID(a.geom, 4326), ST_SETSRID(b.geom, 4326)) WHERE  b.cd_grid = cast(%s as integer);
       CREATE INDEX fdump_%s_gid_idx ON fdump_%s USING btree (gid);
       CREATE INDEX fdump_%s_geom_idx ON fdump_%s USING GIST (geom);', 
@@ -84,7 +84,7 @@ CREATE OR REPLACE FUNCTION inserto_array_agg(grid int)
 $func$
 BEGIN
    EXECUTE format('
-        INSERT INTO malhav2.proc2_malhav3 (original_gid, original_layer, cd_grid, area, geom) 
+        INSERT INTO malhav2.proc2_malhav2 (original_gid, original_layer, cd_grid, area, geom) 
         SELECT
         array_agg(b.original_gid) original_gid,
         array_agg(original_layer) original_layer,
@@ -93,7 +93,7 @@ BEGIN
         ST_Union(ST_Force2d(a.geom)) geom
         FROM split_polygons_%s a 
         LEFT JOIN fdump_%s b ON ST_Intersects(ST_Buffer(ST_Force2d(a.geom)::geography, -5), ST_Force2d(b.geom)) 
-        GROUP BY bin, cd_mun, cd_bioma, cd_grid;', grid, grid);
+        GROUP BY bin, cd_grid;', grid, grid);
 END
 $func$;
 
