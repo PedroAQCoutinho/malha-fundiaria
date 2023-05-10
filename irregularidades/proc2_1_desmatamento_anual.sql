3c085a61e41b7af6976d640613c22146a8f572c4
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-10 12:15:50 -0300

    Update proc2_1_desmatamento_anual.sql

--- irregularidades/proc2_1_desmatamento_anual.sql
+++ irregularidades/proc2_1_desmatamento_anual.sql
@@ -5,7 +5,7 @@
 DROP TABLE IF EXISTS irregularidades.proc21_malha_uso_anual;
 CREATE TABLE irregularidades.proc21_malha_uso_anual (
 cat_fund int NULL,
-car int NULL, 
+id_car_break int NULL, 
 uso int NULL,
 area decimal NULL
 );
@@ -14,7 +14,7 @@ CREATE INDEX proc21_malha_uso_anual_car_idx ON irregularidades.proc21_malha_uso_
 CREATE INDEX proc21_malha_uso_anual_cat_fund_idx ON irregularidades.proc21_malha_uso_anual USING btree (cat_fund);
 CREATE INDEX proc21_malha_uso_anual_uso_idx ON irregularidades.proc21_malha_uso_anual USING btree (uso);
 
-INSERT INTO irregularidades.proc21_malha_uso_anual (cat_fund, car, uso, area)
-SELECT cat_fund, car, uso, sum(count)*0.087 area FROM irregularidades.proc1_malhav2 psar 
+INSERT INTO irregularidades.proc21_malha_uso_anual (cat_fund, id_car_break, uso, area)
+SELECT cat_fund, id_car_break, uso, sum(count)*0.087 area FROM irregularidades.proc1_malhav2 psar 
 WHERE cat_fund IS NOT null
 GROUP BY cat_fund, car, uso;
