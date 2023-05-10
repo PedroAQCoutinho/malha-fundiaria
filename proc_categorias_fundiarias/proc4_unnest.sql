778753ee6763e07f1e5e31be6ca5161d51107b00
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-10 13:18:18 -0300

    Update proc4_unnest.sql

--- proc_categorias_fundiarias/proc4_unnest.sql
+++ proc_categorias_fundiarias/proc4_unnest.sql
@@ -1,6 +1,6 @@
 DROP TABLE malhav2.proc4_unnest;
 CREATE TABLE malhav2.proc4_unnest AS 
-SELECT sub.gid, sub.cd_grid, cd_mun 
+SELECT sub.gid, sub.cd_grid, cd_mun , am_legal
 FROM (SELECT gid, cd_grid, UNNEST(original_gid) original_gid, UNNEST(original_layer) original_layer FROM malhav2.proc2_malhav2 d) sub 
 LEFT JOIN grid.adm2_overlay ao ON sub.original_gid = ao.gid
 WHERE original_layer = 'GRID';
