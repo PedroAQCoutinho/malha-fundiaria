64183d673704fe5f69c250dc70390302419e73a8
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-10 17:25:30 -0300

    Update proc5_cat_fund_2.sql

--- irregularidades/proc5_cat_fund_2.sql
+++ irregularidades/proc5_cat_fund_2.sql
@@ -2,6 +2,7 @@
 -- Caada registro tem um área de sobreposição. Esses registros podem ser agrupados por cat_fund, ou seja, a area total de cada cat_fund no car, agrupado pela nm_cat_fund
 -- Com uma tbaela de tres colunas (car, cat_fund e area) é possivel resgatar a coluna area_car, fazer a proporcao area/area_car e filtrar para > 0.1
 -- Onde as cat_fund forem maiores do que 10%, pegar a maior que indica a area predominante e classifica a propriedade.
+\echo cat fund :var_proc
 
 INSERT INTO irregularidades.proc5_malha_categoria_fundiaria
 SELECT a.gid , foo.nm_agrup FROM dados_brutos.valid_sicar_imovel a 
