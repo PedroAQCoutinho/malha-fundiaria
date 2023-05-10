36a84ba36855f23b334c802ce760feaeaa12743b
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-10 12:36:54 -0300

    Update proc5_cat_fund_1.sql

--- irregularidades/proc5_cat_fund_1.sql
+++ irregularidades/proc5_cat_fund_1.sql
@@ -18,14 +18,12 @@ CREATE INDEX proc5_malha_categoria_fundiaria_nm_agrup_idx ON irregularidades.pro
 
 
 
-CREATE TABLE irregularidades.temp_cat_fund AS  
-SELECT original_gid, nm_agrup, sum(area) area
+CREATE TABLE irregularidades.temp_cat_fund AS 
+SELECT original_gid id_car_original, nm_agrup, sum(area) area
 FROM (SELECT gid, cd_grid, UNNEST(original_gid) original_gid, UNNEST(original_layer) original_layer, nm_agrup, area 
-FROM malhav2.proc6_malha d) sub 
-LEFT JOIN grid.adm2_overlay ao ON sub.original_gid = ao.gid
-WHERE original_layer = 'CAR' and ao.am_legal
+FROM malhav2.proc6_malha d WHERE cd_grid IS NOT NULL AND am_legal) sub 
+WHERE original_layer = 'CAR' 
 GROUP BY original_gid , nm_agrup;
-
 
 
 CREATE INDEX temp_cat_fund_original_gid_idx ON irregularidades.temp_cat_fund USING btree (original_gid);
