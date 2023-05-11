e1b69aa256a3a7d2233d68226f7487939d895c5b
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-11 12:46:41 -0300

    Update proc2_3_desmatamento_anual_2.sql

--- irregularidades/proc2_3_desmatamento_anual_2.sql
+++ irregularidades/proc2_3_desmatamento_anual_2.sql
@@ -1,8 +1,8 @@
 \echo RUN proc23_malha_uso_anual :var_proc
 -- Essa tabela é com dupla contagem por conta do unnest
 INSERT INTO irregularidades.proc23_malha_uso_anual (
-cat_fund, iscar, gid_car, car, uso, area_count, cd_grid, cd_mun, nm_agrup, nm_cat_fund)
-SELECT cat_fund, iscar, gid_car, UNNEST(original_gid) car, 
+cat_fund, iscar, id_car_break, car, uso, area_count, cd_grid, cd_mun, nm_agrup, nm_cat_fund)
+SELECT cat_fund, iscar, id_car_break, UNNEST(original_gid) car, 
 uso, area_count,  cd_grid , cd_mun, nm_agrup, nm_cat_fund
 FROM irregularidades.proc22_malha_uso_anual a 
 WHERE (a.cat_fund % :var_num_proc) = :var_proc
