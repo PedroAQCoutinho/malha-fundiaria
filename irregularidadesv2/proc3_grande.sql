9b81a1ac5a0ce290c666752079fd319c2ddc647d
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-11 13:17:27 -0300

    Update proc3_grande.sql

--- irregularidadesv2/proc3_grande.sql
+++ irregularidadesv2/proc3_grande.sql
@@ -1,9 +1,12 @@
-DROP TABLE IF EXISTS irregularidades.proc4_malha_tamanho_ocupacao;
-CREATE TABLE irregularidades.proc4_malha_tamanho_ocupacao AS 
-SELECT gid car, area, CASE 
-	WHEN area > 2500 THEN TRUE 
-	ELSE false
+DROP TABLE IF EXISTS irregularidadesv2.proc4_car_tamanho_ocupacao;
+CREATE TABLE irregularidades.proc4_car_tamanho_ocupacao AS 
+SELECT origid car, 
+CASE 
+	WHEN sum(area) > 2500 THEN TRUE
+	ELSE FALSE 
 END is_grande
-FROM dados_brutos.valid_sicar_imovel vsi;
+FROM (SELECT unnest(original_gid) origid, UNNEST(original_layer) orilay , area FROM malhav2.proc6_malha pm) foo
+WHERE orilay = 'CAR'
+GROUP BY origid
 
-CREATE INDEX proc4_malha_tamanho_ocupacao_car_idx ON irregularidades.proc4_malha_tamanho_ocupacao USING btree (car);
+CREATE INDEX proc4_car_tamanho_ocupacao_car_idx ON irregularidadesv2.proc4_car_tamanho_ocupacao USING btree (car);
