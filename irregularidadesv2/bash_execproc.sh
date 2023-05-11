f92e72c92737f7878cbad516628c44b432606e23
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-11 13:00:25 -0300

    Update bash_execproc.sh

--- irregularidadesv2/bash_execproc.sh
+++ irregularidadesv2/bash_execproc.sh
@@ -17,3 +17,15 @@ wait
 
 
 
+
+procName=proc2_local_irregular
+export PGPASSWORD='gpp-es@lq'
+
+psql -U $userName -d $databaseName -f ${procName}_1.sql
+
+#If sql 2 exists execute it
+for ((i=0; i < ${numProc}; i++))
+do
+    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_2.sql &
+done
+wait
