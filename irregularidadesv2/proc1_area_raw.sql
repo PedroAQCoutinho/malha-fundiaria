5779227844cd42471a20c6cf6dc9fa268c1a759a
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-12 13:46:03 -0300

    Create proc1_area_raw.sql

---
+++ irregularidadesv2/proc1_area_raw.sql
@@ -0,0 +1,18 @@
+\echo area raw
+
+DROP TABLE irregularidadesv2.proc1_count;
+CREATE TABLE irregularidadesv2.proc1_count 
+(
+cat_fund int NULL,
+desmatamento int NULL,
+uso int NULL,
+area float8 NULL
+);
+
+CREATE INDEX proc1_count_desmatamento_idx ON irregularidadesv2.proc1_count USING btree (desmatamento);
+CREATE INDEX proc1_count_cat_fund_idx ON irregularidadesv2.proc1_count USING btree (cat_fund);
+CREATE INDEX proc1_count_uso_idx ON irregularidadesv2.proc1_count USING btree (uso);
+
+
+INSERT INTO irregularidadesv2.proc1_count (cat_fund, desmatamento, uso, area)
+SELECT cat_fund, desmatamento, uso, count*0.087 FROM public.proc1_row_by_row_mapbiomas_12052023
