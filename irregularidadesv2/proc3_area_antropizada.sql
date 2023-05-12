98c55ff0d049b11df8150ace19ab054543e194fb
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-12 13:46:01 -0300

    Create proc3_area_antropizada.sql

---
+++ irregularidadesv2/proc3_area_antropizada.sql
@@ -0,0 +1,12 @@
+CREATE TABLE irregularidadesv2.proc3_car_area_antropizada AS 
+SELECT id_car_original, sum(area) area_antropizada FROM (
+SELECT pc.cat_fund , pc.desmatamento , pc.uso, UNNEST(pm.original_layer) original_layer , UNNEST(pm.original_gid) id_car_original , 
+pc.area
+FROM irregularidadesv2.proc1_count pc 
+LEFT JOIN malhav2.proc6_malha pm ON pc.cat_fund = pm.gid  
+WHERE cat_fund IS NOT NULL AND pm.is_car AND uso IN (9,15,20,21,24,30,31,39,40,41,46,47,48,62)) foo 
+WHERE original_layer = 'CAR'
+GROUP BY id_car_original, uso 
+
+
+CREATE INDEX proc3_car_area_antropizada_id_car_original_idx ON irregularidadesv2.proc3_car_area_antropizada USING btree (id_car_original);
