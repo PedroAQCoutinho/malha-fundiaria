8fd6aab032a49e4e18258a1575c5da0dda5b151e
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-12 13:45:55 -0300

    Create proc7_id_car.sql

---
+++ irregularidadesv2/proc7_id_car.sql
@@ -0,0 +1,10 @@
+CREATE TABLE irregularidadesv2.proc7_id_car_original AS 
+SELECT DISTINCT id_car_original FROM (
+SELECT pc.cat_fund , pc.desmatamento , pc.uso, UNNEST(pm.original_layer) original_layer , UNNEST(pm.original_gid) id_car_original , 
+pc.area
+FROM irregularidadesv2.proc1_count pc 
+LEFT JOIN malhav2.proc6_malha pm ON pc.cat_fund = pm.gid  
+WHERE cat_fund IS NOT NULL
+) foo WHERE original_layer = 'CAR'
+
+CREATE INDEX proc7_id_car_original_id_car_original_idx ON irregularidadesv2.proc7_id_car_original USING btree (id_car_original);
