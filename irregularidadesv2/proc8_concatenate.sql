aae22ffbe0dfa4711542cff271746ce2ebc55b34
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-18 16:43:02 -0300

    Update proc8_concatenate.sql

--- irregularidadesv2/proc8_concatenate.sql
+++ irregularidadesv2/proc8_concatenate.sql
@@ -11,15 +11,15 @@ vsi.municipio ,
 vsi.cod_munici ,
 pr.is_recente ,
 pr.area_desmatamento_recente ,
+pr.area_desmatamento_antigo ,
 pcaa.area_antropizada ,
-pcto.is_grande ,
+case when vsi.area >= 2500 then TRUE else FALSE end is_grande ,
 pccf.nm_agrup ,
 pclr.is_local_restrito,
 vsi.valid_geom geom FROM irregularidadesv2.proc7_id_car_original pico 
 LEFT JOIN dados_brutos.valid_sicar_imovel vsi ON pico.id_car_original = vsi.gid 
 LEFT JOIN irregularidadesv2.proc2_recente pr ON pico.id_car_original = pr.id_car_original 
 LEFT JOIN irregularidadesv2.proc3_car_area_antropizada pcaa ON pico.id_car_original = pcaa.id_car_original 
-LEFT JOIN irregularidadesv2.proc4_car_tamanho_ocupacao pcto ON pico.id_car_original = pcto.car 
 LEFT JOIN irregularidadesv2.proc5_car_categoria_fundiaria pccf ON pico.id_car_original = pccf.car 
 LEFT JOIN irregularidadesv2.proc6_car_local_restrito pclr ON pico.id_car_original = pclr.car;
 
