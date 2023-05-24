219eeb7d5624e6a37fe8ed1c87f97f6e9c13443f
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-24 08:32:40 -0300

    Update rasterize.sh

--- auxiliar/rasterize.sh
+++ auxiliar/rasterize.sh
@@ -9,3 +9,8 @@ gdal_rasterize  -a gid -of GTiff -a_srs EPSG:4326 -co "COMPRESS=DEFLATE" -co "PR
 
 
 gdal_rasterize  -a cd -of GTiff -a_srs EPSG:4326 -co "COMPRESS=DEFLATE" -co "PREDICTOR=2" -co "ZLEVEL=9" -co "BIGTIFF=IF_SAFER" -co "NUM_THREADS=32" -co "TILED=YES" -te -74.8973961 -34.7917510 -33.0613262 7.9120915 -ts 155239 158459 -ot UInt32 PG:"user=postgres dbname=malha_fundiaria port=5433 password=gpp-es@lq" -sql "select cd, geom from public.car_classificado" car_classificado.tif > log 2>&1 &
+
+
+gdal_rasterize  -a cd -of GTiff -a_srs EPSG:4326 -co "COMPRESS=DEFLATE" -co "PREDICTOR=2" -co "ZLEVEL=9" -co "BIGTIFF=IF_SAFER" -co "NUM_THREADS=32" -co "TILED=YES" -te -74.8973961 -34.7917510 -33.0613262 7.9120915 -ts 155239 158459 -ot UInt32 PG:"user=postgres dbname=malha_fundiaria port=5433 password=gpp-es@lq" -sql "select cd_mun::integer cd, geom from geo_adm.pa_br_limitemunicipal_250_2015_ibge_4674" /home/arquivos/dados_espaciais/limites_administrativos/pa_br_limiteMunicipios_30m_2006_ibge_4326.tif > log 2>&1 &
+
+
