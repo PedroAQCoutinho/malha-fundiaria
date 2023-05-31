0647b138109fbd1b04c67b38ace5eb26e7b5181b
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-05-31 16:18:23 -0300

    Update bash_execproc.sh

--- proc_categorias_fundiarias/bash_execproc.sh
+++ proc_categorias_fundiarias/bash_execproc.sh
@@ -3,6 +3,16 @@ databaseName=malha_fundiaria
 numProc=8
 
 
+export PGPASSWORD='gpp-es@lq'
+procName=proc5_malha
+psql -U $userName -d $databaseName -f ${procName}_1.sql
+
+#If sql 2 exists execute it
+for ((i=0; i < ${numProc}; i++))
+do
+    psql -U $userName -d $databaseName -v var_num_proc=$numProc -v var_proc=$i -f ${procName}_2.sql &
+done
+wait
 
 
 export PGPASSWORD='gpp-es@lq'
