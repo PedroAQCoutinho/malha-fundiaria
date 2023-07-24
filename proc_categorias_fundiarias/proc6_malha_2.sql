681c4f4d1042c37a213dab58126f1be3acc78406
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-07-24 16:26:12 -0300

    Update proc6_malha_2.sql

--- proc_categorias_fundiarias/proc6_malha_2.sql
+++ proc_categorias_fundiarias/proc6_malha_2.sql
@@ -1,18 +1,19 @@
 \echo RUN proc6_malha :var_proc
 
 
-
 INSERT INTO malhav2.proc6_malha (gid, cd_mun, am_legal, original_gid ,original_layer,cd_grid,area, is_car,is_faixafronteira,is_militar,
 is_massadagua,is_quilombola,is_ucpi,is_ucus,is_ucusapa,is_ti,is_imovel,is_gleba,is_gleba_fed, is_gleba_est, is_assentamento,
 original_layer_label,nm_cat_fund,nm_agrup,floresta, pfloresta, agropecuario, pagropecuario,
-nao_vegetada, pnao_vegetada, corpo_dagua,  pcorpo_dagua, area_prioridade_alta, area_prioridade_extrem_alta,area_prioridade_muito_alta,
+nao_vegetada, pnao_vegetada, corpo_dagua,  pcorpo_dagua, antropizada, pantropizada, nao_antropizada, pnao_antropizada, area_prioridade_alta, area_prioridade_extrem_alta,area_prioridade_muito_alta,
 indice_circulariade, geom)
 WITH foo AS (SELECT
   cat_fund,
   SUM(CASE WHEN uso IN (1,3,4,5,49,10,11,12,32,29,50,13) THEN count ELSE 0 END)*0.087 AS floresta,
   sum(CASE WHEN uso IN (14,15,18,19,39,20,40,62,41,36,46,47,48,9,21) THEN count ELSE 0 END)*0.087 AS agropecuario,
   sum(CASE WHEN uso IN (22,23,24,30,25) THEN count ELSE 0 END)*0.087 AS nao_vegetada,
-  sum(CASE WHEN uso IN (26,33,31,27) THEN count ELSE 0 END)*0.087 AS corpo_dagua
+  sum(CASE WHEN uso IN (26,33,31,27) THEN count ELSE 0 END)*0.087 AS corpo_dagua,
+  SUM(CASE WHEN uso IN (9,15,20,21,24,30,31,39,40,41,46,47,48,62) THEN count ELSE 0 END)*0.087 AS antropizada,
+  SUM(CASE WHEN uso IN (3,4,5,49,11,12,32,29,50,13,23,25,33,27) THEN count ELSE 0 END)*0.087 AS nao_antropizada,
   FROM public.proc1_row_by_row_mapbiomas_15052023 prbrm 
 GROUP BY cat_fund),
 bar AS (
@@ -35,6 +36,10 @@ CASE WHEN nao_vegetada IS NOT NULL THEN nao_vegetada ELSE 0 END nao_vegetada,
 CASE WHEN nao_vegetada/area > 1 THEN 1 WHEN nao_vegetada/area IS NULL THEN 0 ELSE nao_vegetada/area END  pnao_vegetada, 
 CASE WHEN corpo_dagua IS NOT NULL THEN corpo_dagua ELSE 0 END corpo_dagua,  
 CASE WHEN corpo_dagua/area > 1 THEN 1 WHEN corpo_dagua/area IS NULL THEN 0 ELSE corpo_dagua/area END  pcorpo_dagua, 
+CASE WHEN antropizada IS NOT NULL THEN antropizada ELSE 0 END antropizada,  
+CASE WHEN antropizada/area > 1 THEN 1 WHEN antropizada/area IS NULL THEN 0 ELSE antropizada/area END  pantropizada, 
+CASE WHEN nao_antropizada IS NOT NULL THEN nao_antropizada ELSE 0 END nao_antropizada,  
+CASE WHEN nao_antropizada/area > 1 THEN 1 WHEN nao_antropizada/area IS NULL THEN 0 ELSE nao_antropizada/area END  pnao_antropizada, 
 area_prioridade_alta,
 area_prioridade_extrem_alta,
 area_prioridade_muito_alta,
@@ -44,6 +49,6 @@ FROM malhav2.proc5_malha a
 LEFT JOIN auxiliares.nomenclatura_categorias_e_agrupamentos_fundiarios b 
 ON a.original_layer_label = b.original_layer
 LEFT JOIN foo ON foo.cat_fund = a.gid 
-LEFT JOIN bar ON bar.cat_fund = a.gid 
+LEFT JOIN bar ON bar.cat_fund = a.gid
 WHERE (a.gid % :var_num_proc) = :var_proc
 
