519ab1765508423535e500b7efcf71976b0daa66
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-07-24 16:26:14 -0300

    Update proc6_malha_1.sql

--- proc_categorias_fundiarias/proc6_malha_1.sql
+++ proc_categorias_fundiarias/proc6_malha_1.sql
@@ -1,3 +1,5 @@
+\echo RUN proc6_malha_1
+
 
 DROP TABLE malhav2.proc6_malha;
 CREATE TABLE malhav2.proc6_malha (
@@ -33,6 +35,10 @@ CREATE TABLE malhav2.proc6_malha (
     pnao_vegetada float8 NULL,
     corpo_dagua float8 NULL, 
     pcorpo_dagua float8 NULL, 
+    antropizada float8 null,
+	pantropizada float8 null,
+    nao_antropizada float8 null,
+	pnao_antropizada float8 null,
     area_prioridade_alta float8 null, 
     area_prioridade_extrem_alta float8 null,
     area_prioridade_muito_alta float8 null ,
