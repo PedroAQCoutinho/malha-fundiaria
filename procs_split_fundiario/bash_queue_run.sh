8f70c1e582467ae9852a41dd6b879347ca071003
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-08-28 15:59:33 -0300

    Update bash_queue_run.sh

--- procs_split_fundiario/bash_queue_run.sh
+++ procs_split_fundiario/bash_queue_run.sh
@@ -4,7 +4,7 @@ exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>log.out 2>&1
 
-N=4  # Amount of jobs to run in parallel
+N=16  # Amount of jobs to run in parallel
 T=0  # Counter for amount of jobs
 Q=() # Job queue
 FILE='cd_grid.txt'
