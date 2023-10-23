e4e05a8bb8c518260b6f1d84e48008c29252e1af
Pedro Alves Quilici Coutinho <30662095+PedroAQCoutinho@users.noreply.github.com>
2023-10-23 12:25:32 -0300

    Update proc4_unnest.sql

--- proc_categorias_fundiarias/proc4_unnest.sql
+++ proc_categorias_fundiarias/proc4_unnest.sql
@@ -1,7 +1,7 @@
 \echo RUN proc4_unnest
 
-DROP TABLE malhav2.proc4_unnest;
-CREATE TABLE malhav2.proc4_unnest AS 
+DROP TABLE malhav2.proc4_unnest_v3;
+CREATE TABLE malhav2.proc4_unnest_v3 AS 
 SELECT sub.gid, sub.cd_grid, cd_mun , am_legal
 FROM (SELECT gid, cd_grid, UNNEST(original_gid) original_gid, UNNEST(original_layer) original_layer FROM malhav2.proc2_malhav2 d) sub 
 LEFT JOIN grid.adm2_overlay ao ON sub.original_gid = ao.gid
@@ -10,6 +10,6 @@ WHERE original_layer = 'GRID';
 
 
 
-CREATE INDEX proc4_unnest_gid_idx ON malhav2.proc4_unnest USING btree (gid);
-CREATE INDEX proc4_unnest_cd_grid_idx ON malhav2.proc4_unnest USING btree (cd_grid);
-CREATE INDEX proc4_unnest_cd_mun_idx ON malhav2.proc4_unnest USING btree (cd_mun);
+CREATE INDEX proc4_unnest_v3_gid_idx ON malhav2.proc4_unnest_v3 USING btree (gid);
+CREATE INDEX proc4_unnest_v3_cd_grid_idx ON malhav2.proc4_unnest_v3 USING btree (cd_grid);
+CREATE INDEX proc4_unnest_v3_cd_mun_idx ON malhav2.proc4_unnest_v3 USING btree (cd_mun);
